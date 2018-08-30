88e1cf08203b395e34935126a383645a067f0e78
David Jiang <djiang86@binghamton.edu>
2018-08-30 22:06:07 +0000

    getting kth from the last element with 2 pointers

---
+++ LinkedList/FindingKthFromLastElementWith2Pointers.java
@@ -0,0 +1,24 @@
+package LinkedList;
+
+public class FindingKthFromLastElementWith2Pointers {
+    public static void main(String[] args) {
+
+    }
+
+    private static LinkedListNode nthToLast(LinkedListNode head, int k) {
+        LinkedListNode p1 = head;
+        LinkedListNode p2 = head;
+
+        for (int i = 0; i < k; i++) {
+            if (p1 == null) return null;
+            p1 = p1.next;
+        }
+
+        while (p1 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return p2;
+    }
+}
