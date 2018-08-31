e0bc12b5a8830c00c131b8f56af2caa98005ee7e
David Jiang <djiang86@binghamton.edu>
2018-08-31 23:35:51 +0000

    checking if a binary tree is balanced

---
+++ TreesAndGraphs/CheckBalanceBinaryTree.java
@@ -0,0 +1,35 @@
+package TreesAndGraphs;
+
+public class CheckBalanceBinaryTree {
+    public int checkHeight(TreeNode root) {
+        if (root == null) return -1;
+
+        int leftHeight = checkHeight(root.left);
+        if (leftHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+        int rightHeight = checkHeight(root.right);
+        if (rightHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+        int heightDiff = leftHeight - rightHeight;
+
+        if (Math.abs(heightDiff) > 1) {
+            return Integer.MIN_VALUE;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != Integer.MIN_VALUE;
+    }
+
+    private class TreeNode {
+        private int value;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int value) {
+            this.value = value;
+        }
+    }
+}
