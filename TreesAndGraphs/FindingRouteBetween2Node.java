0c38de6bf4ba1bbe4db3cc21f58c51c64875295f
David Jiang <djiang86@binghamton.edu>
2018-08-31 21:21:04 +0000

    finding path between 2 nodes with breadth-first

---
+++ TreesAndGraphs/FindingRouteBetween2Node.java
@@ -0,0 +1,42 @@
+package TreesAndGraphs;
+
+import java.util.LinkedList;
+
+public class FindingRouteBetween2Node {
+    enum State {Unvisited, Visited, Visiting}
+
+    public boolean search(Graph g, Node start, Node end) {
+        if (start == end) return true;
+
+        LinkedList<Node> q = new LinkedList<>();
+
+        for (Node n: g.getNodes()) {
+            n.state = State.Unvisited;
+        }
+
+        start.state = State.Visiting;
+        q.add(start);
+        Node u;
+
+        while (!q.isEmpty()){
+            u = q.removeFirst();
+
+            if (u != null) {
+                for (Node v: u.getAdjacents()) {
+                    if (v.state == State.Unvisited) {
+                        if (v == end) {
+                            return true;
+                        } else {
+                            v.state = State.Visiting;
+                            q.add(v);
+                        }
+                    }
+                }
+
+                u.state = State.Visited;
+            }
+        }
+        
+        return false;
+    }
+}
---
+++ TreesAndGraphs/Node.java
@@ -0,0 +1,10 @@
+package TreesAndGraphs;
+
+public class Node {
+    public FindingRouteBetween2Node.State state;
+    private Node[] adjacents;
+
+    public Node[] getAdjacents() {
+        return adjacents;
+    }
+}
