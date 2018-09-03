1cd53b4d2bdd63dab6d2c9bd35c5c7f8bf7e6b43
David Jiang <djiang86@binghamton.edu>
2018-09-03 20:43:56 +0000

    CD, Song, and user for jukebox

---
+++ ObjectOrientedDesign/Jukebox/Song.java
@@ -0,0 +1,4 @@
+package ObjectOrientedDesign.Jukebox;
+
+public class Song {
+}
---
+++ ObjectOrientedDesign/Jukebox/User.java
@@ -0,0 +1,27 @@
+package ObjectOrientedDesign.Jukebox;
+
+public class User {
+    private String name;
+    private Long id;
+
+    public User(String name, Long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
