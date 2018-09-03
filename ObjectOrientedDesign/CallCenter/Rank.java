9c968caf469f8206488dc863cfd3da749fd2755b
David Jiang <djiang86@binghamton.edu>
2018-09-03 20:24:55 +0000

    Call Handler as the body of the program

--- ObjectOrientedDesign/CallCenter/Rank.java
+++ ObjectOrientedDesign/CallCenter/Rank.java
@@ -1,5 +1,14 @@
 package ObjectOrientedDesign.CallCenter;
 
 public enum Rank {
-    Director, Manager, Respondent
+    Director (0), Manager (1), Respondent (2);
+    private int value;
+
+    Rank(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
