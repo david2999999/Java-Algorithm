f0a96baec112d8964ff3716d98f18b0873820f27
David Jiang <djiang86@binghamton.edu>
2018-09-03 23:56:07 +0000

    different conversation classes

---
+++ ObjectOrientedDesign/ChatServer/GroupChat.java
@@ -0,0 +1,6 @@
+package ObjectOrientedDesign.ChatServer;
+
+public class GroupChat extends Conversation{
+    public void removeParticipant(User user) {}
+    public void addParticipant(User user) {}
+}
