7e830692c951fa5b87b6ec476caa742ac8063f71
David Jiang <djiang86@binghamton.edu>
2018-09-03 20:53:07 +0000

    jukebox complete

--- ObjectOrientedDesign/Jukebox/CD.java
+++ ObjectOrientedDesign/Jukebox/CD.java
@@ -1,5 +1,5 @@
 package ObjectOrientedDesign.Jukebox;
 
 public class CD {
-    
+    // data for id, artist, songs
 }
--- ObjectOrientedDesign/Jukebox/Song.java
+++ ObjectOrientedDesign/Jukebox/Song.java
@@ -1,4 +1,5 @@
 package ObjectOrientedDesign.Jukebox;
 
 public class Song {
+    // data for id, cd can be null, title, length
 }
