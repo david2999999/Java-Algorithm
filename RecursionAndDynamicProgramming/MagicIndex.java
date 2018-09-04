db17c7b0558360f62771ba99cf017b381eb06ce6
David Jiang <djiang86@binghamton.edu>
2018-09-04 23:36:30 +0000

    finding the magic number in array

---
+++ RecursionAndDynamicProgramming/MagicIndex.java
@@ -0,0 +1,21 @@
+package RecursionAndDynamicProgramming;
+
+public class MagicIndex {
+    private int magicFast(int[] array) {
+        return magicFast(array, 0, array.length - 1);
+    }
+
+    private int magicFast(int[] array, int start, int end) {
+        if (start > end) return -1;
+
+        int mid = (start + end) / 2;
+
+        if (array[mid] == mid) {
+            return mid;
+        } else if (array[mid] > mid) {
+            return magicFast(array, start, mid - 1);
+        } else {
+            return magicFast(array, mid + 1, end);
+        }
+    }
+}
