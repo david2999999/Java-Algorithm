f6c523e68e668534968ba3d80f7f4ca16fd3843f
David Jiang <djiang86@binghamton.edu>
2018-09-04 23:26:46 +0000

    robot in a grid with dynamic programming

--- RecursionAndDynamicProgramming/RobotInAGrid.java
+++ RecursionAndDynamicProgramming/RobotInAGrid.java
@@ -24,15 +24,4 @@ public class RobotInAGrid {
 
         return false;
     }
-
-
-    private class Point{
-        private int row;
-        private int col;
-
-        public Point(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-    }
 }
