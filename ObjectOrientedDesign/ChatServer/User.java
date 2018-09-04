8f12c0b61fa4b1d7f680e2a3c8c6953d48fa3cf5
David Jiang <djiang86@binghamton.edu>
2018-09-04 00:06:44 +0000

    class for user

--- ObjectOrientedDesign/ChatServer/User.java
+++ ObjectOrientedDesign/ChatServer/User.java
@@ -1,4 +1,58 @@
 package ObjectOrientedDesign.ChatServer;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class User {
+    private int id;
+    private UserStatus status = null;
+
+    private HashMap<Integer, PrivateChat> privateChats;
+    private ArrayList<GroupChat> groupChats;
+
+    private HashMap<Integer, AddRequest> receivedAddRequests;
+    private HashMap<Integer, AddRequest> sentAddRequests;
+
+    private HashMap<Integer, User> contacts;
+
+    private String accountName;
+    private String fullName;
+
+    public User(int id, String accountName, String fullName) {
+        this.id = id;
+        this.accountName = accountName;
+        this.fullName = fullName;
+    }
+
+
+    // methods include
+    // sent messages to user
+    // sent message to group
+    // add contact
+    // receive add request
+    // sent add request
+    // remove add request
+    // remove user
+    // add conversation for group chat
+    // add conversation for private chat
+
+    public void setStatus(UserStatus status) {
+        this.status = status;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
 }
