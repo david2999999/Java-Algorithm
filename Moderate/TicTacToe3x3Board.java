7b7e7ac6bfaf26c325e7a38d91c7a04602561f26
David Jiang <djiang86@binghamton.edu>
2018-09-08 17:00:04 +0000

    checking for win in 3x3 board

---
+++ Moderate/TicTacToe3x3Board.java
@@ -0,0 +1,35 @@
+package Moderate;
+
+public class TicTacToe3x3Board {
+    private enum Piece {Empty, Red, Blue}
+
+    private Piece hasWon(Piece[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            // checks row
+            if (hasWinner(board[i][0], board[i][1], board[i][2])) {
+                return board[i][0];
+            }
+
+            // checks col
+            if (hasWinner(board[0][i], board[1][i], board[2][i])) {
+                return board[0][i];
+            }
+
+            if (hasWinner(board[0][0], board[1][1], board[2][2])) {
+                return board[0][0];
+            }
+
+            if (hasWinner(board[0][2], board[1][1], board[2][0])) {
+                return board[0][2];
+            }
+        }
+
+        return Piece.Empty;
+    }
+
+    private boolean hasWinner(Piece p1, Piece p2, Piece p3) {
+        if (p1 == Piece.Empty) return false;
+
+        return p1 == p2 && p2 == p3;
+    }
+}
