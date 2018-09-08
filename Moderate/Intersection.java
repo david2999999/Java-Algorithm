703d652bb0b81a9e9c5825644ba6cc552d9a20b0
David Jiang <djiang86@binghamton.edu>
2018-09-08 14:56:29 +0000

    swapping points and also checking if a point is between 2 lines

--- Moderate/Intersection.java
+++ Moderate/Intersection.java
@@ -2,6 +2,27 @@ package Moderate;
 
 public class Intersection {
 
+
+    private boolean isBetween(double start, double middle, double end) {
+        if (start > end) {
+            return end <= middle && middle <= start;
+        } else {
+            return start <= middle && middle <= end;
+        }
+    }
+
+    private boolean isBetween(Point start, Point middle, Point end) {
+        return isBetween(start.x, middle.x, end.x) && isBetween(start.y, middle.y, end.y);
+    }
+
+    private void swap(Point one, Point two) {
+        double x = one.x;
+        double y = one.y;
+
+        one.setLocation(two.x, two.y);
+        two.setLocation(x, y);
+    }
+
     private class Line {
         public double slope, yIntercept;
 
