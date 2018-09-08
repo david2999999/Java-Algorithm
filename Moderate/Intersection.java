b8e3b3ba15781279517991bf0443d0984c9ee12e
David Jiang <djiang86@binghamton.edu>
2018-09-08 15:20:43 +0000

    finding intersection of 2 lines

--- Moderate/Intersection.java
+++ Moderate/Intersection.java
@@ -1,6 +1,36 @@
 package Moderate;
 
 public class Intersection {
+    private Point intersect(Point start1, Point end1, Point start2, Point end2) {
+        if (start1.x > end2.x) swap(start1, end1);
+        if (start2.x > end2.x) swap(start2, end2);
+        if (start1.x > start2.x) {
+            swap(start1, start2);
+            swap(end1, end2);
+        }
+
+        Line line1 = new Line(start1, end1);
+        Line line2 = new Line(start2, end2);
+
+        if (line1.slope == line2.slope) {
+            if (line1.yIntercept == line2.yIntercept && isBetween(start1, start2, end1)) {
+                return start2;
+            }
+
+            return null;
+        }
+
+        double x = (line2.yIntercept - line1.yIntercept) / (line1.slope - line2.slope);
+        double y = line1.slope * x + line1.yIntercept;
+
+        Point intersection = new Point(x, y);
+
+        if (isBetween(start1, intersection, end1) && isBetween(start2, intersection, end2)) {
+            return intersection;
+        }
+
+        return null;
+    }
 
 
     private boolean isBetween(double start, double middle, double end) {
