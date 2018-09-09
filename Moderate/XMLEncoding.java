3dd43f7e9347931bb640664b41805fee900bccb6
David Jiang <djiang86@binghamton.edu>
2018-09-09 01:10:13 +0000

    encoding xml

---
+++ Moderate/XMLEncoding.java
@@ -0,0 +1,54 @@
+package Moderate;
+
+public class XMLEncoding {
+    private void encode(Element root, StringBuilder sb) {
+        encode(root.getNameCode(), sb);
+        for (Attribute attr: root.attributes) {
+            encode(attr, sb);
+        }
+
+        encode("0", sb);
+
+        if (root.value != null && !root.value.equals("")) {
+            encode(root.value, sb);
+        } else {
+            for (Element child: root.children) {
+                encode(child, sb);
+            }
+        }
+
+        encode("0", sb);
+    }
+
+
+    private void encode(String v, StringBuilder sb) {
+        sb.append(v);
+        sb.append(" ");
+    }
+
+    private void encode(Attribute attribute, StringBuilder sb) {
+        encode(attribute.getTagCode(), sb);
+        encode(attribute.value, sb);
+    }
+
+
+    private class Element {
+        public Attribute[] attributes;
+        public String nameCode;
+        public String value;
+        public Element[] children;
+
+        public String getNameCode() {
+            return nameCode;
+        }
+    }
+
+    private class Attribute {
+
+        public String value;
+
+        public String getTagCode() {
+            return "Tag Code";
+        }
+    }
+}
