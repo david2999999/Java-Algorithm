1dafdd1ff49d52b4d53e006899c1d995673e977d
David Jiang <djiang86@binghamton.edu>
2018-09-11 23:57:19 +0000

    palindrome with #

---
+++ Redo/CheckPalindromeWithCounter.java
@@ -0,0 +1,36 @@
+package Redo;
+
+public class CheckPalindromeWithCounter {
+    private boolean checkPalindrome(String phrase) {
+        int countOdd = 0;
+        int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+
+        for (char c: phrase.toCharArray()) {
+            int val = getNumericValue(c);
+
+            if (val != -1) {
+                table[val]++;
+
+                if (table[val] % 2 == 1) {
+                    countOdd++;
+                } else {
+                    countOdd--;
+                }
+            }
+        }
+
+        return countOdd <= 1;
+    }
+
+    private int getNumericValue(char c) {
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+
+        if (a <= val && val <= z) {
+            return val;
+        }
+
+        return -1;
+    }
+}
