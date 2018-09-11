f1d2875bc7f9452ba54b248998b61b4011d1a649
David Jiang <djiang86@binghamton.edu>
2018-09-11 04:05:25 +0000

    langton ant, position and orientation class

---
+++ Moderate/LangtonAnt/Orientation.java
@@ -0,0 +1,17 @@
+package Moderate.LangtonAnt;
+
+public enum Orientation {
+    left, up, right, down;
+
+    public Orientation getTurn(boolean clockwise) {
+        if (this == left) {
+            return clockwise ? up : down;
+        } else if (this == up) {
+            return clockwise ? right : left;
+        } else if (this == right) {
+            return clockwise ? down : up;
+        } else {
+            return clockwise ? left : right;
+        }
+    }
+}
