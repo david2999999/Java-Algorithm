c290862fbd841962cd1fc88a786be5bdfca71b4d
David Jiang <djiang86@binghamton.edu>
2018-09-11 17:22:13 +0000

    pair of sum in arraylist

---
+++ Moderate/PairWithSums/Pair.java
@@ -0,0 +1,11 @@
+package Moderate.PairWithSums;
+
+public class Pair {
+    private int num1;
+    private int num2;
+
+    public Pair(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+}
