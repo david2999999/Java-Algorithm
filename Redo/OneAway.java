fbb0b2229fbc56c99c56edcb12e1223ac060b1c0
David Jiang <djiang86@binghamton.edu>
2018-09-12 01:03:14 +0000

    one away combined

--- Redo/OneAway.java
+++ Redo/OneAway.java
@@ -2,7 +2,15 @@ package Redo;
 
 public class OneAway {
     private boolean isOneAway(String str1, String str2) {
+        if (str1.length() == str2.length())
+            return oneEditReplacement(str1, str2);
+        else if (str1.length() + 1 == str2.length()) {
+            return oneEditInsert(str1, str2);
+        } else if (str1.length() == str2.length() + 1) {
+            return oneEditInsert(str2, str1);
+        }
 
+        return false;
     }
 
     private boolean oneEditInsert(String str1, String str2) {
@@ -35,5 +43,36 @@ public class OneAway {
         }
 
         return true;
+    }
+
+    private boolean oneAwayCombined(String str1, String str2) {
+        if (Math.abs(str1.length() - str2.length()) > 1) {
+            return false;
+        }
+
+        String s1 = str1.length() < str2.length() ? str1 : str2;
+        String s2 = str1.length() < str2.length() ? str2 : str1;
+
+
+        int index1 = 0;
+        int index2 = 0;
+        boolean foundDifference = false;
+
+        while (index1 < s1.length() && index2 < s2.length()) {
+            if (s1.charAt(index1) != s2.indexOf(index2)) {
+                if (foundDifference) return false;
+                foundDifference = true;
+
+                if (s1.length() == s2.length()) {
+                    index1++;
+                }
+            } else {
+                index1++;
+            }
+
+            index2++;
+        }
+
+        return false;
     }
 }
