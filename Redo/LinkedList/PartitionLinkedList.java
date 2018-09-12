776e8b4ed1c7b30c6017947b9c27e14969d85728
David Jiang <djiang86@binghamton.edu>
2018-09-12 13:48:54 +0000

    partion array space optimize

--- Redo/LinkedList/PartitionLinkedList.java
+++ Redo/LinkedList/PartitionLinkedList.java
@@ -39,4 +39,26 @@ public class PartitionLinkedList {
         leftEnd.next = rightStart;
         return leftStart;
     }
+
+    private LinkedListNode partitionList(LinkedListNode node, int k) {
+        LinkedListNode head = node;
+        LinkedListNode tail = node;
+
+        while (node != null) {
+            LinkedListNode next = node.next;
+
+            if (node.data < k) {
+                node.next = head;
+                head = node;
+            } else {
+                tail.next = node;
+                tail = node;
+            }
+
+            node = next;
+        }
+        tail.next = null;
+
+        return head;
+    }
 }
