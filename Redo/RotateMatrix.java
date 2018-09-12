909df51dc8587d9fc52bd1a27968eae273687f0b
David Jiang <djiang86@binghamton.edu>
2018-09-12 01:50:34 +0000

    rotate matrix

---
+++ Redo/RotateMatrix.java
@@ -0,0 +1,28 @@
+package Redo;
+
+public class RotateMatrix {
+    private boolean rotateMatrix(int[][] matrix) {
+        if (matrix.length != matrix[0].length) return false;
+
+        int n = matrix.length;
+
+        for (int layer = 0; layer < n; layer++) {
+            int first = layer;
+            int last = n - 1 - first;
+
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+
+                int top = matrix[first][i];
+
+                matrix[first][i] = matrix[last - offset][first];
+
+                matrix[last - offset][first] = matrix[last][last - offset];
+
+                matrix[last][last - offset] = matrix[i][last];
+
+                matrix[i][last] = top;
+            }
+        }
+    }
+}
