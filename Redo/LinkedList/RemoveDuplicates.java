7ee8615a3ce7407d7c4c754832ee726b5ca36dbb
David Jiang <djiang86@binghamton.edu>
2018-09-12 12:45:06 +0000

    removing duplicates inside linkedlist without buffer

--- Redo/LinkedList/RemoveDuplicates.java
+++ Redo/LinkedList/RemoveDuplicates.java
@@ -20,4 +20,22 @@ public class RemoveDuplicates {
             node = node.next;
         }
     }
+
+    private void removeDuplicatesWithoutBuffer(LinkedListNode head) {
+        LinkedListNode current = head;
+
+        while (current != null) {
+            LinkedListNode runner = current;
+
+            while (runner.next != null) {
+                if (runner.next.data == current.data) {
+                    runner.next = runner.next.next;
+                } else {
+                    runner = runner.next;
+                }
+            }
+
+            current = current.next;
+        }
+    }
 }
