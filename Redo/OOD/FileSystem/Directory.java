6d87775bb144cc84be8909293cd57c9c2738f079
David Jiang <djiang86@binghamton.edu>
2018-09-17 22:18:54 +0000

    entry for file system

---
+++ Redo/OOD/FileSystem/Directory.java
@@ -0,0 +1,11 @@
+package Redo.OOD.FileSystem;
+
+public class Directory extends Entry{
+
+    public Directory(Directory parent, String name) {
+        super(parent, name);
+    }
+
+    public boolean deleteEntry(Entry entry) {
+    }
+}
