f1a98d2ed722b2be4bcba5f1b4b3bbdb55a0c4cb
David Jiang <djiang86@binghamton.edu>
2018-09-17 22:27:33 +0000

    file system complete

--- Redo/OOD/FileSystem/File.java
+++ Redo/OOD/FileSystem/File.java
@@ -1,12 +1,28 @@
 package Redo.OOD.FileSystem;
 
 public class File extends Entry {
-    public File(Directory parent, String name) {
+    private int size;
+    private String content;
+
+    public File(Directory parent, String name, int size) {
         super(parent, name);
+        this.size = size;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
