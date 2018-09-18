97f9020457cd5a165d5af6b027bbb808defe48e4
David Jiang <djiang86@binghamton.edu>
2018-09-18 17:31:20 +0000

    building paren from string

---
+++ Redo/RecursionAndDynamicProgramming/Parens/Paren2.java
@@ -0,0 +1,27 @@
+package Redo.RecursionAndDynamicProgramming.Parens;
+
+import java.util.ArrayList;
+
+public class Paren2 {
+    private void addParen(ArrayList<String> list, int leftRem, int rightRem, char[] str, int index) {
+        if (leftRem < 0 || rightRem < leftRem) return;
+
+        if (leftRem == 0 && rightRem == 0) {
+            list.add(String.copyValueOf(str));
+        } else {
+            str[index] = '(';
+            addParen(list, leftRem - 1, rightRem, str, index + 1);
+
+            str[index] = ')';
+            addParen(list, leftRem, rightRem - 1, str, index + 1);
+        }
+    }
+
+
+    private ArrayList<String> generateParen(int count) {
+        char[] str = new char[count * 2];
+        ArrayList<String> list = new ArrayList<>();
+        addParen(list, count, count, str, 0);
+        return list;
+    }
+}
