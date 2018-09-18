e4f0483263987d908efcde3ff2414b237ca2118a
David Jiang <djiang86@binghamton.edu>
2018-09-18 19:48:49 +0000

    stack of boxes

---
+++ Redo/RecursionAndDynamicProgramming/StackOfBoxes/Box.java
@@ -0,0 +1,9 @@
+package Redo.RecursionAndDynamicProgramming.StackOfBoxes;
+
+public class Box {
+    public int height;
+
+    public boolean canBeAbove(Box bottomBox) {
+        return true;
+    }
+}
