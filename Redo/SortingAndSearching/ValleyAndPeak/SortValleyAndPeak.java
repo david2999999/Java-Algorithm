6d0b50da5d0639896324d280b774a9f512330e0e
David Jiang <djiang86@binghamton.edu>
2018-09-21 01:53:26 +0000

    peaks and valley, sort first

---
+++ Redo/SortingAndSearching/ValleyAndPeak/SortValleyAndPeak.java
@@ -0,0 +1,18 @@
+package Redo.SortingAndSearching.ValleyAndPeak;
+
+import java.util.Arrays;
+
+public class SortValleyAndPeak {
+    private void sort(int[] array) {
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            swap(array, i - 1, i);
+        }
+    }
+
+    private void swap(int[] array, int left, int right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+}
