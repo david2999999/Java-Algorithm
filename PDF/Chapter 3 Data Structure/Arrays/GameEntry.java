9b46aa43de4b85f28598bffb84369026ad91a5e5
David Jiang <djiang86@binghamton.edu>
2018-12-03 13:21:46 +0000

    game entry class

---
+++ PDF/Chapter 3 Data Structure/Arrays/GameEntry.java
@@ -0,0 +1,21 @@
+public class GameEntry {
+    private String name;
+    private int score;
+    
+    public GameEntry(String n, int s) {
+        name = n;
+        score = s;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getScore() {
+        return score;
+    }
+    
+    public String toString() {
+        return "(" + name + ", " + score + ")";
+    }
+}
