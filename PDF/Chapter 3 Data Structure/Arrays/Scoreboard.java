2e00a1764ba29e22d2fc3c3c96b01c7ea5bd4ce6
David Jiang <djiang86@binghamton.edu>
2018-12-03 13:39:49 +0000

    removing element from array with shifting

--- PDF/Chapter 3 Data Structure/Arrays/Scoreboard.java
+++ PDF/Chapter 3 Data Structure/Arrays/Scoreboard.java
@@ -20,4 +20,18 @@ public class Scoreboard {
             board[j] = e;
         }
     }
+    
+    public GameEntry remove(int i) throws IndexOutOfBoundsException {
+        if (i < 0 || i >= numEntries) 
+            throw new IndexOutOfBoundsException("Invalid index: " + i);
+            
+        GameEntry temp = board[i];
+        for (int j = i; j < numEntries - 1; j++) {
+            board[j] = board[j + 1];
+        }
+        
+        board[numEntries - 1] = null;
+        numEntries--;
+        return temp;
+    }
 }
