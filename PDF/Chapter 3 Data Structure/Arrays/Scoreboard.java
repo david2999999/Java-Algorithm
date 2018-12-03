d78485b2c79f166de3b07227e9d42c5d085eff47
David Jiang <djiang86@binghamton.edu>
2018-12-03 13:29:13 +0000

    inserting new game entry to scoreboard

--- PDF/Chapter 3 Data Structure/Arrays/Scoreboard.java
+++ PDF/Chapter 3 Data Structure/Arrays/Scoreboard.java
@@ -5,4 +5,19 @@ public class Scoreboard {
     public Scoreboard(int capacity) {
         board = new GameEntry[capacity];
     }
+    
+    public void add(GameEntry e) {
+        int newScore = e.getScore();
+        
+        if (numEntries < board.length || newScore > board[numEntries - 1].getScore) {
+            if (numEntries < board.length) numEntries++;
+            
+            int j = numEntries - 1;
+            while(j > 0 && board[j - 1].getScore() < newScore) {
+                board[j] = board[j - 1];
+                j--;
+            }
+            board[j] = e;
+        }
+    }
 }
