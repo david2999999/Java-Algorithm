75cbc046621c92565a6de60b21def6215fbc8ca7
David Jiang <djiang86@binghamton.edu>
2018-12-04 21:02:33 +0000

    removing first element from linkedlist

--- PDF/Chapter 3 Data Structure/List/SinglyLinkedList.java
+++ PDF/Chapter 3 Data Structure/List/SinglyLinkedList.java
@@ -57,4 +57,14 @@ public class SinglyLinkedList<E> {
         tail = newest;
         size++;
     }
+    
+    public E removeFirst() {
+        if (isEmpty()) return null;
+        E answer = head.getElement();
+        head = head.getNext();
+        size--;
+        
+        if (size == 0) tail = null;
+        return answer;
+    }
 }
