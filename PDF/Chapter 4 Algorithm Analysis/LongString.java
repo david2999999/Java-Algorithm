4e155661f806ed3239b8aba6e28f3909c661fd00
David Jiang <djiang86@binghamton.edu>
2018-12-05 13:54:22 +0000

    comparing concatenating strings vs stringbuilder

---
+++ PDF/Chapter 4 Algorithm Analysis/LongString.java
@@ -0,0 +1,18 @@
+public class LongString {
+    public static String repeat1(char c, int n) {
+        String answer = "";
+        for (int j = 0; j < n; j++) {
+            answer += c;
+        }
+        
+        return answer;
+    }
+    
+    public static String repeat2(char c, int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < n; j++) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
