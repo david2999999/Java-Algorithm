34212e876a6e33efdb82e620dceedb25801870a7
David Jiang <djiang86@binghamton.edu>
2018-12-05 14:47:08 +0000

    O(n) prefix avg

--- PDF/Chapter 4 Algorithm Analysis/PrefixAverages.java
+++ PDF/Chapter 4 Algorithm Analysis/PrefixAverages.java
@@ -1,4 +1,8 @@
 public class PrefixAverages {
+    // The running time of implementation prefixAverage1 is given by the sum of these
+    // terms. The first term is O(1), the second and third terms are O(n), and the fourth
+    // term is O(n2). By a simple application of Proposition 4.8, the running time of
+    // prefixAverage1 is O(n2).
     public static double[] prefixAverage1(double[] x) {
         int n = x.length;
         double[] a = new double[n];
@@ -9,6 +13,22 @@ public class PrefixAverages {
             a[j] = total / (j + 1);
         }
         
+        return a;
+    }
+    
+    // The running time of algorithm prefixAverage2 is given by the sum of the five terms.
+    // The first and last are O(1) and the remaining three are O(n). By a simple application
+    // of Proposition 4.8, the running time of prefixAverage2 is O(n), which is much
+    // better than the quadratic time of algorithm prefixAverage1.
+    public static double[] prefixAverage2(double[] x) {
+        int n = x.length;
+        double[] a = new double[n];
+        double total = 0;
+        
+        for (int j = 0; j < n; j++) {
+            total += x[j];
+            a[j] = total / (j + 1);
+        }
         return a;
     }
 }
