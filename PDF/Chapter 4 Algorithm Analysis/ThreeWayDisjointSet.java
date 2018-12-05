2f6317c25e297d679670dea7db92abe4921650be
David Jiang <djiang86@binghamton.edu>
2018-12-05 14:20:34 +0000

    O(n^2) 3 disjoint sets

--- PDF/Chapter 4 Algorithm Analysis/ThreeWayDisjointSet.java
+++ PDF/Chapter 4 Algorithm Analysis/ThreeWayDisjointSet.java
@@ -10,6 +10,23 @@ public class ThreeWayDisjointSet {
                         return false;
         return true;
     }
+    
+    //To account for the overall running time, we examine the time spent executing
+    // each line of code. The management of the for loop over A requires O(n) time. The
+    // management of the for loop over B accounts for a total of O(n2) time, since that
+    // loop is executed n different times. The test a == b is evaluated O(n2) times. The
+    // rest of the time spent depends upon how many matching (a,b) pairs exist. As we
+    // have noted, there are at most n such pairs; therefore, the management of the loop
+    // over C and the commands within the body of that loop use at most O(n2) time. By
+    // our standard application of Proposition 4.8, the total time spent is O(n2).
+    public static boolean disjoint2(int[] groupA, int[] groupB, int[] groupC) {
+        for (int a: groupA)
+            for (int b: groupB)
+                if (a == b) 
+                    for (int c: groupC)
+                        if (a == c)
+                            return false;
+        return true;
+    }
 }
 
-//
