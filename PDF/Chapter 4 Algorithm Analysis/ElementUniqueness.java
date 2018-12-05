60cb4e8bbeaaf4509138957a96decf08748ec5cd
David Jiang <djiang86@binghamton.edu>
2018-12-05 14:29:46 +0000

    element uniqueness in O(nlogn)

--- PDF/Chapter 4 Algorithm Analysis/ElementUniqueness.java
+++ PDF/Chapter 4 Algorithm Analysis/ElementUniqueness.java
@@ -8,4 +8,17 @@ public class ElementUniqueness {
         
         return true;
     }
+    
+    public static boolean unique2(int[] data) {
+        int n = data.length;
+        int[] temp = Arrays.copyOf(data, n);
+        Arrays.sort(temp);
+        
+        for (int j = 0; j < n - 1; j++) {
+            if (temp[j] == temp[j + 1])
+                return false;
+        }
+        
+        return true;
+    }
 }
