7b813cc19542ea5e1a3ff0d99540d6676d1440d3
David Jiang <djiang86@binghamton.edu>
2018-12-10 21:51:46 +0000

    reverse iterative

---
+++ PDF/Chapter 1/ReverseIterative.java
@@ -0,0 +1,12 @@
+public class ReverseIterative {
+    
+    public static void reverse(int[] data) {
+        int low = 0, high = data.length - 1;
+    
+        while (low < high) {
+            int temp = data[low];
+            data[low++] = data[high];
+            data[high--] = temp;
+        }
+    }
+}
