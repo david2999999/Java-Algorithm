14a67213dbc5f8ac029b9686b97a9d85d8abd9b1
David Jiang <djiang86@binghamton.edu>
2018-12-11 20:48:00 +0000

    get/set element in arraylist

--- PDF/Chapter 7 List and Iterator ADTs/List/ArrayList.java
+++ PDF/Chapter 7 List and Iterator ADTs/List/ArrayList.java
@@ -10,4 +10,21 @@ public class ArrayList<E> implements List<E> {
     
     public int size() { return size; }
     public boolean isEmpty() { return size == 0; }
+    
+    public E get(int i) throws IndexOutOfBoundsException {
+        checkIndex(i, size);
+        return data[i];
+    }
+    
+    public E set(int i, E e) throws IndexOutOfBoundsException {
+        checkIndex(i, size);
+        E temp = data[i];
+        data[i] = e;
+        return temp;
+    }
+    
+    protected void checkIndex(int i, int n) throws IndexOutOfBoundsException {
+        if (i < 0 || i >= n)
+            throw new IndexOutOfBoundsException("Illegal index: " + i);
+    }
 }
