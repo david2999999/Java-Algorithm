02b71c0c2dfea65bc17ed7a2c8d49a0a0f07e638
David Jiang <djiang86@binghamton.edu>
2018-12-11 19:59:51 +0000

    stack queue

--- PDF/Chapter 6 Stacks Queues Deques/Queue/ArrayQueue.java
+++ PDF/Chapter 6 Stacks Queues Deques/Queue/ArrayQueue.java
@@ -16,4 +16,25 @@ public class ArrayQueue<E> implements Queue<E> {
     public boolean isEmpty() {
         return sz == 0;
     }
+    
+    public void enqueue(E e) throws IllegalStateException{
+        if (sz == data.length) throw new IllegalStateException("Queue is full");
+        int avail = (f + sz) % data.length;
+        data[avail] = e;
+        sz++;
+    }
+    
+    public E first() {
+        if (isEmpty()) return null;
+        return data[f];
+    }
+    
+    public E dequeue() {
+        if (isEmpty()) return null;
+        E answer = data[f];
+        data[f] = null;
+        f = (f + 1) % data.length;
+        sz--;
+        return answer;
+    }
 }
