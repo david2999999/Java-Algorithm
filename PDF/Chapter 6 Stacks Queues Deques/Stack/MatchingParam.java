a244bd24c4fd2a607e5ebc6283c11018e2c41253
David Jiang <djiang86@binghamton.edu>
2018-12-11 13:41:57 +0000

    matching param with stack

---
+++ PDF/Chapter 6 Stacks Queues Deques/Stack/MatchingParam.java
@@ -0,0 +1,20 @@
+public class MatchingParam {
+    public static boolean isMatched(String expression) {
+        final String opening = "({[";
+        final String closing = ")}]";
+        Stack<Character> buffer = new LinkedStack<>();
+        
+        for (char c: expression.toCharArray()) {
+            if (opening.indexOf(c) != -1)
+                buffer.push(c);
+            else if (closing.indexOf(c) != -1) {
+                if (buffer.isEmpty())
+                    return false;
+                if (closing.indexOf(c) != opening.indexOf(buffer.pop()))
+                    return false;
+            }
+        } 
+        
+        return buffer.isEmpty();
+    }
+}
