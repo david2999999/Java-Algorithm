a1a81f723040f77842935cd8a85118991f6da67b
David Jiang <djiang86@binghamton.edu>
2018-12-13 15:12:36 +0000

    node that implements position

---
+++ PDF/Chapter 7 List and Iterator ADTs/Positional List/LinkedPositionalList.java
@@ -0,0 +1,24 @@
+public class LinkedPositionalList implements PositionalList<E> {
+    private static class Node<E> implements Position<E> {
+        private E element;
+        private Node<E> prev;
+        private Node<E> next;
+        public Node(E e, Node<E> p, Node<E> n) {
+            element = e;
+            prev = p;
+            next = n;
+        }
+        
+        public E getElement() throws IllegalStateException {
+            if (next == null)
+                throw new IllegalStateException("Position is not longer valid");
+            return element;
+        }
+        
+        public E getElement() { return element; }
+        public Node<E> getPrev() { return prev; }
+        public Node<E> getNext() { return next; }
+        public void setPrev(Node<E> p) { prev = p; }
+        public void setNext(Node<E> n) { next = n; }
+    }
+}
