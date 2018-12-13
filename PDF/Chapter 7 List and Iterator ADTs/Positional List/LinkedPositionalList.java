7d8ee6d77897efc7f3d0986d162a156454d7107e
David Jiang <djiang86@binghamton.edu>
2018-12-13 15:25:03 +0000

    private method inside LPL to verify the position of a node

--- PDF/Chapter 7 List and Iterator ADTs/Positional List/LinkedPositionalList.java
+++ PDF/Chapter 7 List and Iterator ADTs/Positional List/LinkedPositionalList.java
@@ -21,4 +21,28 @@ public class LinkedPositionalList implements PositionalList<E> {
         public void setPrev(Node<E> p) { prev = p; }
         public void setNext(Node<E> n) { next = n; }
     }
+    
+    private Node<E> header;
+    private Node<E> trailer;
+    private int size = 0;
+    
+    public LinkedPositionalList() {
+        header = new Node<>(null, null, null);
+        trailer = new Node<>(null, header, null);
+        header.setNext(trailer);
+    }
+    
+    private Node<E> validate(Position<E> p) throws IllegalArgumentException {
+        if (!(p instanceof Node)) throw new IllegalArgumentException("Invalid p");
+        Node<E> node = (Node<E>) p;
+        if (node.getNext() == null)
+            throw new IllegalArgumentException("p is no longer in the list");
+        return node;
+    }
+    
+    private Position<E> position(Node<E> node) {
+        if (node == header || node == trailer)
+            return null;
+        return node;
+    }
 }
