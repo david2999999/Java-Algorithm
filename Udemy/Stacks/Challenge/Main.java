339ae0a285c4251429d1732d73c9c5de8de71757
David Jiang <djiang86@binghamton.edu>
2018-12-15 00:59:14 +0000

    stack challenge palindrome

---
+++ Udemy/Stacks/Challenge/Main.java
@@ -0,0 +1,41 @@
+package Stacks.Challenge;
+
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // should return true
+        System.out.println(checkForPalindrome("abccba"));
+        // should return true
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw?"));
+        // should return true
+        System.out.println(checkForPalindrome("I did, did I?"));
+        // should return false
+        System.out.println(checkForPalindrome("hello"));
+        // should return true
+        System.out.println(checkForPalindrome("Don't nod"));
+    }
+
+    public static boolean checkForPalindrome(String string) {
+
+        LinkedList<Character> stack = new LinkedList<Character>();
+        StringBuilder stringNoPunctuation = new StringBuilder(string.length());
+        String lowerCase = string.toLowerCase();
+
+        for (int i = 0; i < lowerCase.length(); i++) {
+            char c = lowerCase.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                stringNoPunctuation.append(c);
+                stack.push(c);
+            }
+        }
+
+        StringBuilder reversedString = new StringBuilder(stack.size());
+        while (!stack.isEmpty()) {
+            reversedString.append(stack.pop());
+        }
+
+        return reversedString.toString().equals(stringNoPunctuation.toString());
+    }
+}
