2241f3b37973b1d7b481975d95d60ef470bcc700
David Jiang <djiang86@binghamton.edu>
2018-12-15 15:05:11 +0000

    challenge 2

---
+++ Udemy/HashMap/Challenge2/Main.java
@@ -0,0 +1,60 @@
+package HashMap.Challenge2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList<Employee> employees = new LinkedList<>();
+        employees.add(new Employee("Jane", "Jones", 123));
+        employees.add(new Employee("John", "Doe", 5678));
+        employees.add(new Employee("Mike", "Wilson", 45));
+        employees.add(new Employee("Mary", "Smith", 5555));
+        employees.add(new Employee("John", "Doe", 5678));
+        employees.add(new Employee("Bill", "End", 3948));
+        employees.add(new Employee("Jane", "Jones", 123));
+
+        employees.forEach(e -> System.out.println(e));
+
+        HashMap<Integer, Employee> employeeTable = new HashMap<>();
+        ListIterator<Employee> iterator = employees.listIterator();
+        List<Employee> remove = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            Employee employee = iterator.next();
+            if (employeeTable.containsKey(employee.getId())) {
+                remove.add(employee);
+            }
+            else {
+                employeeTable.put(employee.getId(), employee);
+            }
+        }
+
+        for (Employee employee: remove) {
+            employees.remove(employee);
+        }
+
+        System.out.println("-------------------------");
+        employees.forEach(e -> System.out.println(e));
+
+
+
+//        int[] nums = new int[10];
+//        int[] numsToAdd = { 59382, 43, 6894, 500, 99, -58 };
+//        for (int i = 0; i < numsToAdd.length; i++) {
+//            nums[hash(numsToAdd[i])] = numsToAdd[i];
+//        }
+//        for (int i = 0; i < nums.length; i++) {
+//            System.out.println(nums[i]);
+//        }
+    }
+
+    public static int hash(int value) {
+        return Math.abs(value % 10);
+    }
+}
