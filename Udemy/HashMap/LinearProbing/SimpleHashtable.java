94e01694ebe237b962ea7bc7255d9f3bfaf3c49c
David Jiang <djiang86@binghamton.edu>
2018-12-15 12:02:40 +0000

    removing employees in linear probing

--- Udemy/HashMap/LinearProbing/SimpleHashtable.java
+++ Udemy/HashMap/LinearProbing/SimpleHashtable.java
@@ -40,6 +40,17 @@ public class SimpleHashtable {
         return hashtable[hashedKey].employee;
     }
 
+    public Employee remove(String key) {
+        int hashedKey = findKey(key);
+        if (hashedKey == -1) {
+            return null;
+        }
+
+        Employee employee = hashtable[hashedKey].employee;
+        hashtable[hashedKey] = null;
+        return employee;
+    }
+
     private int hashKey(String key) {
         return key.length() % hashtable.length;
     }
@@ -65,11 +76,12 @@ public class SimpleHashtable {
             hashedKey = (hashedKey + 1) % hashtable.length;
         }
 
-        if (stopIndex == hashedKey) {
-            return -1;
+        if (hashtable[hashedKey] != null &&
+                hashtable[hashedKey].key.equals(key)) {
+            return hashedKey;
         }
         else {
-            return hashedKey;
+            return -1;
         }
 
     }
