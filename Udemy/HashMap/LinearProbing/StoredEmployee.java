4627f3fe878c3063f15f5106d894cff1c0c847ce
David Jiang <djiang86@binghamton.edu>
2018-12-15 02:08:57 +0000

    linear probing

---
+++ Udemy/HashMap/LinearProbing/StoredEmployee.java
@@ -0,0 +1,12 @@
+package HashMap.LinearProbing;
+
+public class StoredEmployee {
+
+    public String key;
+    public Employee employee;
+
+    public StoredEmployee(String key, Employee employee) {
+        this.key = key;
+        this.employee = employee;
+    }
+}
