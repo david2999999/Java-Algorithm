7c26ac96cdb8996c70219573d1b3714937497cac
David Jiang <djiang86@binghamton.edu>
2018-12-15 12:43:01 +0000

    chaining in hashmap

---
+++ Udemy/HashMap/Chaining/StoredEmployee.java
@@ -0,0 +1,13 @@
+package HashMap.Chaining;
+
+public class StoredEmployee {
+
+    public String key;
+    public Employee employee;
+
+    public StoredEmployee(String key, Employee employee) {
+        this.key = key;
+        this.employee = employee;
+    }
+
+}
