80c345eca8d0a90b807855fda1236bb2f5de0cf4
David Jiang <djiang86@binghamton.edu>
2018-12-15 12:17:01 +0000

    rehashing for remove method

--- Udemy/HashMap/LinearProbing/SimpleHashtable.java
+++ Udemy/HashMap/LinearProbing/SimpleHashtable.java
@@ -48,6 +48,15 @@ public class SimpleHashtable {
 
         Employee employee = hashtable[hashedKey].employee;
         hashtable[hashedKey] = null;
+
+        StoredEmployee[] oldHashtable = hashtable;
+        hashtable = new StoredEmployee[oldHashtable.length];
+        for (int i = 0; i < oldHashtable.length; i++) {
+            if (oldHashtable[i] != null) {
+                put(oldHashtable[i].key, oldHashtable[i].employee);
+            }
+        }
+
         return employee;
     }
 
