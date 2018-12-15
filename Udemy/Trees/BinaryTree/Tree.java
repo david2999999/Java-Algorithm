474ead7c5bbed332391c1a1d3c4a835761940ec8
David Jiang <djiang86@binghamton.edu>
2018-12-15 17:53:35 +0000

    inserting values into tree

---
+++ Udemy/Trees/BinaryTree/Tree.java
@@ -0,0 +1,17 @@
+package Trees.BinaryTree;
+
+public class Tree {
+
+    private TreeNode root;
+
+    public void insert(int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+        }
+        else {
+            root.insert(value);
+        }
+    }
+
+
+}
