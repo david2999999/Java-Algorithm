4ff4bb330a950df13207899fb80381f7f6afd3f8
David Jiang <djiang86@binghamton.edu>
2018-12-16 22:33:49 +0000

    binode converting binary tree to linkedlist

---
+++ Hard/BiNode/BiNode.java
@@ -0,0 +1,11 @@
+package Hard.BiNode;
+
+public class BiNode {
+    public BiNode node1;
+    public BiNode node2;
+
+    public BiNode(BiNode node1, BiNode node2) {
+        this.node1 = node1;
+        this.node2 = node2;
+    }
+}
