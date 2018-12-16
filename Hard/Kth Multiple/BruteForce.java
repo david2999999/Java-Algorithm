c018cd8c7170341f773d2bc227c7d1ba9d24c5b4
David Jiang <djiang86@binghamton.edu>
2018-12-16 18:06:51 +0000

    find kth multiple brute force

---
+++ Hard/Kth Multiple/BruteForce.java
@@ -0,0 +1,38 @@
+package Hard.KthMultiple;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BruteForce {
+    public int getKthMagicNumber(int k) {
+        ArrayList<Integer> possibilities = allPossibleKFactors(k);
+        Collections.sort(possibilities);
+        return possibilities.get(k);
+    }
+
+
+
+    private ArrayList<Integer> allPossibleKFactors(int k) {
+        ArrayList<Integer> values = new ArrayList<>();
+
+        for (int a = 0; a <= k; a++) {
+            int powA = (int) Math.pow(3, a);
+            for (int b = 0; b <= k; b++) {
+                int powB = (int) Math.pow(5, b);
+                for (int c = 0; c <= k; c++) {
+                    int powC = (int) Math.pow(7, c);
+                    int value = powA * powB * powC;
+
+                    if (value < 0 || powA == Integer.MAX_VALUE
+                            || powB == Integer.MAX_VALUE || powC == Integer.MAX_VALUE) {
+                        value = Integer.MAX_VALUE;
+                    }
+
+                    values.add(value);
+                }
+            }
+        }
+
+        return values;
+    }
+}
