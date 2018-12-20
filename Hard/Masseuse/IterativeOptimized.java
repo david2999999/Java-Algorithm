3e021d0a6fe703cd6da9970d09120b02a28122a7
David Jiang <djiang86@binghamton.edu>
2018-12-20 02:34:15 +0000

    masseuse iterative optimized

---
+++ Hard/Masseuse/IterativeOptimized.java
@@ -0,0 +1,18 @@
+package Hard.Masseuse;
+
+public class IterativeOptimized {
+    public int maxMinutes(int[] massages) {
+        int oneAway = 0;
+        int twoAway = 0;
+
+        for (int i = massages.length - 1; i >= 0; i--) {
+            int bestWith = massages[i] + twoAway;
+            int bestWithout = oneAway;
+            int current = Math.max(bestWith, bestWithout);
+            twoAway = oneAway;
+            oneAway = current;
+        }
+
+        return oneAway;
+    }
+}
