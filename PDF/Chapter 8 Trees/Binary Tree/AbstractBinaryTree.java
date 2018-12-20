6a4157bacfd4740412af70cb71fffba0ac722bcf
David Jiang <djiang86@binghamton.edu>
2018-12-20 15:52:53 +0000

    abstract binary tree

---
+++ PDF/Chapter 8 Trees/Binary Tree/AbstractBinaryTree.java
@@ -0,0 +1,34 @@
+public abstract class AbstractBinaryTree<E> extends AbstractTree<E> implements BinaryTree<E> {
+    public Position<E> sibling(Position<E> p) {
+        Position<E> parent = parent(p);
+        if (parent == null) return null;
+        
+        if (p == left(parent)) {
+            return right(parent);
+        }  else {
+            return left(parent);
+        }
+    }
+    
+    public int numChildren(Position<E> p) {
+        int count = 0;
+        if (left(p) != null) 
+            count++;
+            
+        if (right(p) != null) 
+            count++;
+            
+        return count;
+    }
+    
+    public Iterable<Position<E>> children(Position<E> p) {
+        List<Position<E>> snapshot = new ArrayList<>(2);
+        if (left(p) != null) 
+            snapshot.add(left(p));
+            
+        if (right(p) != null) 
+            snapshot.add(right(p));
+            
+        return snapshot;
+    }
+}
