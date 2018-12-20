67a78ba2ace16f0549ce3431ce98da1fc03bad28
David Jiang <djiang86@binghamton.edu>
2018-12-20 18:49:31 +0000

    add right, left node. Also, set node

--- PDF/Chapter 8 Trees/Binary Tree/LinkedBinaryTree.java
+++ PDF/Chapter 8 Trees/Binary Tree/LinkedBinaryTree.java
@@ -90,5 +90,42 @@ public class LinkedBinaryTree<E> extends AbstractBinaryTree<E> {
         return node.getRight();
     }
 
-
+    public Position<E> addRoot(E e) throws IllegalStateException {
+        if (!isEmpty()) throw new IllegalStateException("Tree is not empty");
+        
+        
+        root = createNode(e, null, null, null);
+        size = 1;
+        return root;
+    }
+    
+    public Position<E> addLeft(Position<E> p, E e) throws IllegalArgumentException {
+        Node<E> parent = validate(p);
+        if (parent.getLeft() != null) {
+            throw new IllegalArgumentException("p already has a left child");
+        }
+        Node<E> child = createNode(e, parent, null, null);
+        parent.setLeft(child);
+        size++;
+        return child;
+    }
+    
+    public Position<E> addRight(Position<E> p, E e) throws IllegalArgumentException {
+        Node<E> parent = validate(p);
+        if (parent.getLeft() != null) {
+            throw new IllegalArgumentException("p already has a right child");
+        }
+        Node<E> child = createNode(e, parent, null, null);
+        parent.setRight(child);
+        size++;
+        return child;
+    }
+    
+    public E set(Position<E> p, E e) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        E temp = node.getElement();
+        node.setElement(e);
+        return temp;
+    }
+    
 }
