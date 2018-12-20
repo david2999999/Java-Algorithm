337891cba2e1be5468a65abce972b1e5c10b83db
David Jiang <djiang86@binghamton.edu>
2018-12-20 18:37:51 +0000

    basic methods to retrieve parent, left, right of node

--- PDF/Chapter 8 Trees/Binary Tree/LinkedBinaryTree.java
+++ PDF/Chapter 8 Trees/Binary Tree/LinkedBinaryTree.java
@@ -46,4 +46,49 @@ public class LinkedBinaryTree<E> extends AbstractBinaryTree<E> {
         }
     }
     
+    protected Node<E> createNode(E e, Node<E> parent, Node<E> left, Node<E> right) {
+        return new Node<E>(e, parent, left, right);
+    }
+    
+    
+    protected Node<E> root = null;
+    private int size = 0;
+    
+    public LinkedBinaryTree() {}
+    
+    protected Node<E> validate(Position<E> p) throws IllegalArgumentException {
+        if (!(p instanceof Node)) 
+            throw new IllegalArgumentException("Not valid position type");
+            
+        Node<E> node = (Node<E>) p;
+        if (node.getParent() == node) 
+            throw new IllegalArgumentException("p is no longer in the tree");
+        
+        return node;
+    }
+    
+    public int size() {
+        return size;
+    }
+    
+    public Position<E> root() {
+        return root;
+    }
+    
+    public Position<E> parent(Position<E> p) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return node.getParent();
+    }
+    
+    public Position<E> left(Position<E> p) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return node.getLeft();
+    }
+    
+    public Position<E> right(Position<E> p) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return node.getRight();
+    }
+
+
 }
