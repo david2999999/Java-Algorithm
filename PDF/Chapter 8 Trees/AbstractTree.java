4ed3058aabcd7f2256b474265b4efb11d0a0cff6
David Jiang <djiang86@binghamton.edu>
2018-12-20 14:09:41 +0000

    finding the height of a node on a tree

--- PDF/Chapter 8 Trees/AbstractTree.java
+++ PDF/Chapter 8 Trees/AbstractTree.java
@@ -21,4 +21,14 @@ public abstract class AbstractTree<E> implements Tree<E> {
         else 
             return 1 + depth(parent(p));
     }
+    
+    public int height(Position<E> p) {
+        int h = 0;
+        
+        for (Position<E> c: children(p)) {
+            h = Math.max(h, 1 + height(c));
+        }
+        
+        return h;
+    }
 }
