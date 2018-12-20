f3919702c62431895fb5ac57171edbdf87ecbbdc
David Jiang <djiang86@binghamton.edu>
2018-12-20 01:09:35 +0000

    finding the most time recursive

---
+++ Hard/Masseuse/RecursiveSolution.java
@@ -0,0 +1,19 @@
+package Hard.Masseuse;
+
+public class RecursiveSolution {
+    public int maxMinutes(int[] messages) {
+        return maxMinutes(messages, 0);
+    }
+
+
+    private int maxMinutes(int[] messages, int index) {
+        if (index >= messages.length) {
+            return 0;
+        }
+
+        int bestWith = messages[index] + maxMinutes(messages, index + 2);
+        int bestWithout = messages[index];
+
+        return Math.max(bestWith, bestWithout);
+    }
+}
