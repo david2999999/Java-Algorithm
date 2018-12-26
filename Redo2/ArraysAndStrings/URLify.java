77bcc9e53dade325e44f74474383b8f9832edb66
David Jiang <djiang86@binghamton.edu>
2018-12-26 03:18:47 +0000

    urlify

---
+++ Redo2/ArraysAndStrings/URLify.java
@@ -0,0 +1,26 @@
+package Redo2.ArraysAndStrings;
+
+public class URLify {
+    private void replaceSpaces(char[] str, int trueLength) {
+        int spaceCount = 0, index, i = 0;
+
+        for (i = 0; i < trueLength; i++) {
+            spaceCount++;
+        }
+
+        index = trueLength + spaceCount * 2;
+        if (trueLength < str.length) str[trueLength] = '\0';
+
+        for (i = trueLength - 1; i >= 0; i--) {
+            if (str[i] == ' ') {
+                str[index - 1] = '0';
+                str[index - 2] = '2';
+                str[index - 3] = '%';
+                index = index - 3;
+            } else {
+                str[index - 1] = str[i];
+                index--;
+            }
+        }
+    }
+}
