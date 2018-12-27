68a39892c317c39b22c2f47e7000e920aa9b313f
David Jiang <djiang86@binghamton.edu>
2018-12-27 20:06:19 +0000

    abstract priority queue

--- PDF/Chapter 9 Priority Queues/AbstractPriorityQueue.java
+++ PDF/Chapter 9 Priority Queues/AbstractPriorityQueue.java
@@ -24,4 +24,25 @@ public abstract class AbstractPriorityQueue<K, V> implements PriorityQueue<K, V>
         }
     }
     
+    private Comparator<K> comp;
+    
+    protected AbstractPriorityQueue(Comparator<K> c) { comp = c; }
+    
+    protected AbstractPriorityQueue() { this(new DefaultComparator<K>()); }
+    
+    protected int compare(Entry<K, V> a, Entry<K, V> b) {
+        return comp.compare(a.getKey(), b.getKey());
+    }
+    
+    protected boolean checkKey(K key) throws IllegalArgumentException {
+        try {
+            return (comp.compare(key,key) == 0); 
+        } catch (ClassCastException e) {
+            throw new IllegalArgumentException("Incompatible key");
+        }
+    }
+    
+    public boolean isEmpty() {
+        return size() == 0;
+    }
 }
