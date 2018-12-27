e8d8459527b500db751d3ceae06bad09e15f532a
David Jiang <djiang86@binghamton.edu>
2018-12-27 20:31:36 +0000

    sorted priority queue

---
+++ PDF/Chapter 9 Priority Queues/SortedPriorityQueue.java
@@ -0,0 +1,35 @@
+public class SortedPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
+    private PositionalList<Entry<K,V>> list = new LinkedPositionalList<>();
+    
+    public SortedPriorityQueue() { super(); }
+    public SortedPriorityQueue(Comparator<K> comp) { super(comp); }
+    
+    public Entry<K, V> insert(K key, V value) throws IllegalArgumentException {
+        checkKey(key);
+        Entry<K, V> newest = new PQEntry<>(key, value);
+        Position<Entry<K, V>> walk = list.last();
+        
+        while(walk != null && compare(newest, walk.getElement()) < 0)
+            walk = list.before(walk);
+        
+        if (walk == null) 
+            list.addFirst(newest);
+        else 
+            list.addAfter(walk, newest);
+            
+        return newest;
+    }
+    
+    public Entry<K, V> min() {
+        if (list.isEmpty()) return null;
+        
+        return list.first().getElement();
+    }
+    
+    public Entry<K, V> removeMin() {
+        if (list.isEmpty()) return null;
+        return list.remove(list.first());
+    }
+    
+    public int size() { return list.size(); }
+}
