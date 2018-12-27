6f768759b6bbf0f7a4c31709341f501a09f259f0
David Jiang <djiang86@binghamton.edu>
2018-12-27 18:10:08 +0000

    inorder traversal binary tree

--- PDF/Chapter 8 Trees/Binary Tree/AbstractBinaryTree.java
+++ PDF/Chapter 8 Trees/Binary Tree/AbstractBinaryTree.java
@@ -32,5 +32,26 @@ public abstract class AbstractBinaryTree<E> extends AbstractTree<E> implements B
         return snapshot;
     }
     
-
+    private void inorderSubtree(Position<E> p, List<Position<E>> snapshot) {
+        if (left(p) != null) {
+            inorderSubtree(left(p), snapshot);
+        }
+        
+        snapshot.add(p);
+        
+        if (right(p) != null) {
+            inorderSubtree(right(p), snapshot);
+        }
+    }
+    
+    public Iterable<Position<E>> inorder() {
+        List<Position<E>> snapshot = new ArrayList<>();
+        if (!isEmpty())
+            inorderSubtree(root(), snapshot);
+        return snapshot;
+    }
+    
+    public Iterable<Position<E>> positions() {
+        return inorder();
+    }
 }
