94b92e8ce8458e164247fb5664e5aab8d790c681
David Jiang <djiang86@binghamton.edu>
2018-12-27 21:14:18 +0000

    swap and also upheap elements

--- PDF/Chapter 9 Priority Queues/HeapPriorityQueue.java
+++ PDF/Chapter 9 Priority Queues/HeapPriorityQueue.java
@@ -10,4 +10,19 @@ public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
     
     protected boolean hasLeft(int j) { return left(j) < heap.size(); }
     protected boolean hasRight(int j) { return right(j) < heap.size(); }
+    
+    protected void swap(int i, int j) {
+        Entry<K, V> temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
+    
+    protected void upheap(int j) {
+        while(j > 0) {
+            int p = parent(j);
+            if (compare(heap.get(j), heap.get(p)) >= 0) break;
+            swap(j, p);
+            j = p;
+        }
+    }
 }
