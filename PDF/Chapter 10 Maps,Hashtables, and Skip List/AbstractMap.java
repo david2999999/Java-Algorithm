96c8958bb00afda2c96e1944e2338817d111e017
David Jiang <djiang86@binghamton.edu>
2018-12-28 15:05:50 +0000

    value iterator

--- PDF/Chapter 10 Maps,Hashtables, and Skip List/AbstractMap.java
+++ PDF/Chapter 10 Maps,Hashtables, and Skip List/AbstractMap.java
@@ -37,4 +37,17 @@ public abstract class AbstractMap<K, V> implements Map<K, V> {
     }
     
     public Iterable<K> keySet() { return new KeyIterable(); }
+    
+    private class ValueIterator implements Iterator<V> {
+        private Iterator<Entry<K, V>> entries = entrySet().iterator();
+        public boolean hasNext() { return entries.hasNext(); }
+        public V next() { return entries.next().getValue(); }
+        public void remove() { throw new UnsupportedOperationException(); }
+    }
+    
+    private class ValueIterable implements Iterable<V> {
+        public Iterator<V> iterator() { return new ValueIterator(); }
+    }
+    
+    public Iterable<V> values() { return new ValueIterable(); }
 }
