6e3727ec8d67b50272db5096d855714c143ae8a7
David Jiang <djiang86@binghamton.edu>
2018-12-28 20:27:35 +0000

    put/remove item from hashmultimap

---
+++ PDF/Chapter 10.1 Sets, Multisets, and Multimaps/HashMultimap.java
@@ -0,0 +1,44 @@
+public class HashMultimap<K, V> {
+    Map<K,List<V>> map = new HashMap<>();
+    int total = 0;
+    
+    public HashMultimap() { }
+    
+    public int size() { return total; }
+    public boolean isEmpty() { return (total == 0); }
+    
+    Iterable<V> get(K key) {
+        List<V> secondary = map.get(key);
+        if (secondary != null)
+            return secondary;
+        return new ArrayList<>();
+    }
+    
+    public void put(K key, V value) {
+        List<V> secondary = map.get(key);
+        
+        if (secondary == null) {
+            secondary = new ArrayList<>();
+            map.put(key, secondary);
+        }
+        
+        secondary.add(value);
+        total++;
+    }
+    
+    boolean remove(K key, V value) {
+        boolean wasRemoved = false;
+        List<V> secondary = map.get(key);
+        
+        if (secondary != null) {
+            wasRemoved = secondary.remove(value);
+            if (wasRemoved) {
+                total--;
+                if (secondary.isEmpty())
+                    map.remove(key);
+            }
+        }
+        
+        return wasRemoved;
+    }
+}
