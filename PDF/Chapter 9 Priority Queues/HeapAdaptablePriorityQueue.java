cac7a560e742d7ef572e60368bc491bfd059a146
David Jiang <djiang86@binghamton.edu>
2018-12-28 14:18:35 +0000

    validating an entry, swap and reset entry index

--- PDF/Chapter 9 Priority Queues/HeapAdaptablePriorityQueue.java
+++ PDF/Chapter 9 Priority Queues/HeapAdaptablePriorityQueue.java
@@ -1,1 +1,37 @@
-public class HeapAdaptablePriorityQueue<K, V> extends HeapPriorityQueue<K, V> 
+public class HeapAdaptablePriorityQueue<K, V> 
+        extends HeapPriorityQueue<K, V> implements AdaptablePriorityQueue<K, V> {
+    
+    protected static class AdaptablePQEntry<K, V> extends PQEntry<K, V> {
+        private int index;
+        public AdaptablePQEntry(K key, V value, int j) {
+            super(key, value);
+            index = j;
+        }
+        
+        public int getIndex() { return index; }
+        public void setIndex(int j) { index = j; }
+    }
+    
+    
+    public HeapAdaptablePriorityQueue() { super(); }
+    public HeapAdaptablePriorityQueue(Comparator<K> comp) { super(comp);}
+    
+    protected AdaptablePQEntry<K, V> validate(Entry<K, V> entry) throws IllegalArgumentException{
+        if (!(entry instanceof AdaptablePQEntry))
+            throw new IllegalArgumentException("Invalid entry");
+            
+        AdaptablePQEntry<K, V> locator = (AdaptablePQEntry<K, V>) entry;
+        int j = locator.getIndex();
+        
+        if (j >= heap.size() || heap.get(j) != locator)
+            throw new IllegalArgumentException("Invalid entry");
+            
+        return locator;
+    }
+    
+    protected void swap(int i, int j) {
+        super.swap(i, j);
+        ((AdaptablePQEntry<K,V>) heap.get(i)).setIndex(i);
+        ((AdaptablePQEntry<K,V>) heap.get(j)).setIndex(j);
+    }
+}
