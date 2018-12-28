cf793d2236cff8e485855727753fe804a687cfa0
David Jiang <djiang86@binghamton.edu>
2018-12-28 16:38:51 +0000

    public methods of abstract hashmap

---
+++ PDF/Chapter 10 Maps,Hashtables, and Skip List/Abstract/AbstractHashMap.java
@@ -0,0 +1,30 @@
+public abstract class AbstractHashMap<K, V> extends AbstractMap<K, V> {
+    protected int n = 0;
+    protected int capacity;
+    private int prime;
+    private long scale, shift;
+    
+    public AbstractHashMap(int cap, int p) {
+        prime = p;
+        capacity = cap;
+        Random rand = new Random();
+        scale = rand.nextInt(prime - 1) + 1;
+        shift = rand.nextInt(prime);
+        createTable();
+    }
+    
+    public AbstractHashMap(int cap) { this(cap, 109345121); }
+    public AbstractHashMap() { this(17); }
+    
+    public int size() { return n; }
+    public V get(K key) { return bucketGet(hashValue(key), key); }
+    public V remove(K key) { return bucketRemove(hashValue(key), key); }
+    public V put(K key, V value) {
+        V answer = bucketPut(hashValue(key), key, value);
+        if (n > capacity / 2) {
+            resize(2 * capacity - 1);
+        } 
+        
+        return answer;
+    }
+}
