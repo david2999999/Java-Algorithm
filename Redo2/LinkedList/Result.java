31cfbe6730e3477159af75e439072f115868bdb2
David Jiang <djiang86@binghamton.edu>
2018-12-30 23:31:16 +0000

    result simple class

---
+++ Redo2/LinkedList/Result.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+import CodeLibrary.LinkedListNode;
+
+public class Result {
+    public LinkedListNode tail;
+    public int size;
+
+    public Result(LinkedListNode tail, int size) {
+        this.tail = tail;
+        this.size = size;
+    }
+}
