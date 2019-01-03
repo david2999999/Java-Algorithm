feaad31d7ddda826d61c23f904ff117156df074a
David Jiang <djiang86@binghamton.edu>
2019-01-03 00:32:28 +0000

    node with min

---
+++ Redo2/StacksAndQueues/NodeWithMin.java
@@ -0,0 +1,11 @@
+package StacksAndQueues;
+
+public class NodeWithMin {
+    public int value;   // data/value
+    public int min;     // the current minimum
+
+    public NodeWithMin(int value, int min) {
+        this.value = value;
+        this.min = min;
+    }
+}
