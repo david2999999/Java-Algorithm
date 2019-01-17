58a9c5b5924918de54f1fccb059538ecc096de8e
David Jiang <djiang86@binghamton.edu>
2019-01-17 13:16:10 +0000

    binary search

---
+++ Data Structure and Algorithm PDF 2/Chapter 2 Array/BinarySearch.java
@@ -0,0 +1,23 @@
+public int find(long searchKey)
+    {
+    int lowerBound = 0;
+    int upperBound = nElems-1;
+    
+    int curIn;
+    
+    while(true)
+        {
+        curIn = (lowerBound + upperBound ) / 2;
+        if(a[curIn]==searchKey)
+            return curIn; // found it
+        else if(lowerBound > upperBound)
+            return nElems; // can’t find it
+        else // divide range
+            {
+            if(a[curIn] < searchKey)
+                lowerBound = curIn + 1; // it’s in upper half
+            else
+                upperBound = curIn - 1; // it’s in lower half
+            } // end else divide range
+        } // end while
+    } // end find()
