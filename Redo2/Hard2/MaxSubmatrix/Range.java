ad100c66592c9be9cc40866285178f6e44f9396f
David Jiang <djiang86@binghamton.edu>
2019-01-20 11:19:39 +0000

    range and submatrix class

---
+++ Redo2/Hard2/MaxSubmatrix/Range.java
@@ -0,0 +1,11 @@
+package Hard2.MaxSubmatrix;
+
+public class Range {
+    public int start, end, sum;
+
+    public Range(int start, int end, int sum) {
+        this.start = start;
+        this.end = end;
+        this.sum = sum;
+    }
+}
