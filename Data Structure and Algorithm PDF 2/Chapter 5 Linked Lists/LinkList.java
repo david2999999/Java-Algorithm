73e1b107d22df6f1ac205548c603cc0724ed061a
David Jiang <djiang86@binghamton.edu>
2019-01-21 14:46:24 +0000

    find/delete in linkedlist

--- Data Structure and Algorithm PDF 2/Chapter 5 Linked Lists/LinkList.java
+++ Data Structure and Algorithm PDF 2/Chapter 5 Linked Lists/LinkList.java
@@ -39,4 +39,38 @@ class LinkList
     System.out.println("");
     }
     // -------------------------------------------------------------
-} // end class LinkList
+    public Link find(int key) // find link with given key
+        { // (assumes non-empty list)
+        Link current = first; // start at ‘first’
+        while(current.iData != key) // while no match,
+            {
+            if(current.next == null) // if end of list,
+                return null; // didn’t find it
+            else // not end of list,
+                current = current.next; // go to next link
+            }
+        return current; // found it
+        }
+    // -------------------------------------------------------------
+    public Link delete(int key) // delete link with given key
+        { // (assumes non-empty list)
+        Link current = first; // search for link
+        Link previous = first;
+        while(current.iData != key)
+            {
+            if(current.next == null)
+                return null; // didn’t find it
+            else
+                {
+                previous = current; // go to next link
+                current = current.next;
+                }
+        } // found it
+        
+        if(current == first) // if first link,
+            first = first.next; // change first
+        else // otherwise,
+            previous.next = current.next; // bypass it
+        return current;
+        }
+    } // end class LinkList
