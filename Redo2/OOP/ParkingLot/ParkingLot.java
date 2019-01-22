2aa9cd26368a630f9ff9c8f0265eaabe1cda8db3
David Jiang <djiang86@binghamton.edu>
2019-01-22 00:17:43 +0000

    parking lot

---
+++ Redo2/OOP/ParkingLot/ParkingLot.java
@@ -0,0 +1,16 @@
+package OOP.ParkingLot;
+
+import java.util.logging.Level;
+
+public class ParkingLot {
+    private Level[] levels;
+    private final int NUM_LEVELS = 5;
+
+    public ParkingLot() {
+        levels = new Level[NUM_LEVELS];
+    }
+
+    public boolean parkVehicle(Vehicle vehicle) {
+        return false;
+    }
+}
