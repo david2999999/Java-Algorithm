bdc9a09a9766270922852e5a2e28c04d48538136
David Jiang <djiang86@binghamton.edu>
2019-01-26 00:08:30 +0000

    delete middle node

---
+++ Redo3/LinkedList/DeleteMiddleNode.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+import CodeLibrary.LinkedListNode;
+
+public class DeleteMiddleNode {
+    public boolean deleteNode(LinkedListNode n) {
+        if (n == null || n.next == null) return false;
+
+        LinkedListNode next = n.next;
+        n.data = next.data;
+        n.next = next.next;
+
+        return true;
+    }
+}
