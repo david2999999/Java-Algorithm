6551df38f80ae62d98de366069ae1cdf82c28926
David Jiang <djiang86@binghamton.edu>
2019-01-26 00:08:20 +0000

    solution 2

---
+++ Redo3/LinkedList/SumList/PartialSum.java
@@ -0,0 +1,10 @@
+package LinkedList.SumList;
+
+import CodeLibrary.LinkedListNode;
+
+public class PartialSum {
+    public LinkedListNode sum = null;
+    public int carry = 0;
+
+    public PartialSum() { }
+}
