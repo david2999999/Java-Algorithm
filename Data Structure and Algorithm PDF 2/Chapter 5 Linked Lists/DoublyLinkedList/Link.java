9b644a028d3867f3775b9a4a3c87c6dec5116653
David Jiang <djiang86@binghamton.edu>
2019-01-29 12:27:44 +0000

    node for doubly linked list

---
+++ Data Structure and Algorithm PDF 2/Chapter 5 Linked Lists/DoublyLinkedList/Link.java
@@ -0,0 +1,14 @@
+class Link
+    {
+    public long dData; // data item
+    public Link next; // next link in list
+    public Link previous; // previous link in list
+    // -------------------------------------------------------------
+    public Link(long d) // constructor
+        { dData = d; }
+    // -------------------------------------------------------------
+    public void displayLink() // display this link
+        { System.out.print(dData + “ “); }
+    // -------------------------------------------------------------
+    } // end class Link
+////////////////////////////////////////////////////////////////
