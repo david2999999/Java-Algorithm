58d6ae4da31d825c8637f165b0fe0460dc96d341
David Jiang <djiang86@binghamton.edu>
2019-01-30 04:10:21 +0000

    solution 2

---
+++ Redo3/RecursionAndDynamicProgramming/MagicIndex/Solution2.java
@@ -0,0 +1,29 @@
+package RecursionAndDynamicProgramming.MagicIndex;
+
+public class Solution2 {
+    public int magicFast(int[] array) {
+        return magicFast(array, 0, array.length - 1);
+    }
+
+    private int magicFast(int[] array, int start, int end) {
+        if (end < start) return -1;
+
+        int midIndex = (start + end) / 2;
+        int midValue = array[midIndex];
+
+        if (midValue == midIndex) {
+            return midIndex;
+        }
+
+        int leftIndex = Math.min(midIndex - 1, midValue);
+        int left = magicFast(array, start, leftIndex);
+        if (left >= 0) {
+            return left;
+        }
+
+        int rightIndex = Math.max(midIndex + 1, midValue);
+        int right = magicFast(array, rightIndex, end);
+
+        return right;
+    }
+}
