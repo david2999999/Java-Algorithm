9b13e85959c66ef08d870f16cb01e0f77540901f
David Jiang <djiang86@binghamton.edu>
2019-01-31 01:02:36 +0000

    with memoization

---
+++ Redo3/RecursionAndDynamicProgramming/RecursiveMultiply/Memoization.java
@@ -0,0 +1,32 @@
+package RecursionAndDynamicProgramming.RecursiveMultiply;
+
+public class Memoization {
+    public int minProduct(int a, int b) {
+        int bigger = a < b ? b : a;
+        int smaller = a < b ? a : b;
+
+        int[] memo = new int[smaller + 1];
+        return minProduct(smaller, bigger, memo);
+    }
+
+    private int minProduct(int smaller, int bigger, int[] memo) {
+        if (smaller == 0) {
+            return 0;
+        } else if (smaller == 1) {
+            return bigger;
+        } else if (memo[smaller] > 0) {
+            return memo[smaller];
+        }
+
+        int s = smaller >> 1;
+        int side1 = minProduct(s, bigger, memo);
+        int side2 = side1;
+
+        if (smaller % 2 == 1) {
+            side2 = minProduct(smaller - s, bigger, memo);
+        }
+
+        memo[smaller] = side1 + side2;
+        return memo[smaller];
+    }
+}
