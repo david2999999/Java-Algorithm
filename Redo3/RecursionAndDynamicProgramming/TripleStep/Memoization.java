4d62d740ba47e9b38384bd44bc301675d091cd80
David Jiang <djiang86@binghamton.edu>
2019-01-31 01:04:48 +0000

    with memoization

---
+++ Redo3/RecursionAndDynamicProgramming/TripleStep/Memoization.java
@@ -0,0 +1,25 @@
+package RecursionAndDynamicProgramming.TripleStep;
+
+import java.util.Arrays;
+
+public class Memoization {
+    public int countWays(int n) {
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        return countWays(n, memo);
+    }
+
+    private int countWays(int n, int[] memo) {
+        if (n < 0) {
+            return 0;
+        } else if (n == 0) {
+            return 1;
+        } else if (memo[n] > -1) {
+            return memo[n];
+        } else {
+            memo[n] = countWays(n - 1, memo) + countWays(n - 2, memo) + countWays(n - 3, memo);
+    }
+
+        return memo[n];
+    }
+}
