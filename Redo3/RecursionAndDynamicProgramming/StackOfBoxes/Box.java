73e283a4e3f10bf6583b21be91076186fe872fd5
David Jiang <djiang86@binghamton.edu>
2019-01-31 01:03:27 +0000

    stack of boxes solution 1

---
+++ Redo3/RecursionAndDynamicProgramming/StackOfBoxes/Box.java
@@ -0,0 +1,11 @@
+package RecursionAndDynamicProgramming.StackOfBoxes;
+
+public class Box {
+    public int height;
+    public int width;
+    public int length;
+
+    public boolean canBeAbove(Box bottom) {
+        return true;
+    }
+}
