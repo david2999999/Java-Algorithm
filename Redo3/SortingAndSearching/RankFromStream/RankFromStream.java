cbc5ee1f39822df586ef6aac07ef909dd44554ed
David Jiang <djiang86@binghamton.edu>
2019-02-02 15:07:03 +0000

    rank from stream

---
+++ Redo3/SortingAndSearching/RankFromStream/RankFromStream.java
@@ -0,0 +1,17 @@
+package SortingAndSearching.RankFromStream;
+
+public class RankFromStream {
+    private RankNode root = null;
+
+    public void track(int number) {
+        if (root == null) {
+            root = new RankNode(number);
+        } else {
+            root.insert(number);
+        }
+    }
+
+    public int getRankOfNumber(int number) {
+        return root.getRank(number);
+    }
+}
