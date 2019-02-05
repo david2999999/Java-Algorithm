5f174518e9ca27f091898ad0598eec072936621c
David Jiang <djiang86@binghamton.edu>
2019-02-05 02:21:00 +0000

    trie tree

---
+++ Redo3/CodeLibrary/Trie.java
@@ -0,0 +1,35 @@
+package CodeLibrary;
+
+import java.util.ArrayList;
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie(ArrayList<String> list) {
+        root = new TrieNode();
+        for (String word: list) {
+            root.addWord(word);
+        }
+    }
+
+    public boolean contains(String prefix, boolean exact) {
+        TrieNode lastNode = root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+            lastNode = lastNode.getChild(prefix.charAt(i));
+            if (lastNode == null) {
+                return false;
+            }
+        }
+
+        return !exact || lastNode.terminates();
+    }
+
+    public boolean contains(String prefix) {
+        return contains(prefix, false);
+    }
+
+    public TrieNode getRoot() {
+        return root;
+    }
+}
