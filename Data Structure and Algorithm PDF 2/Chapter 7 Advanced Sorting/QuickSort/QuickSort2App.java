d355b9eca2cb5d561cb07e64444b3b5d75d51389
David Jiang <djiang86@binghamton.edu>
2019-02-05 12:19:18 +0000

    quick sort 2 app

---
+++ Data Structure and Algorithm PDF 2/Chapter 7 Advanced Sorting/QuickSort/QuickSort2App.java
@@ -0,0 +1,19 @@
+class QuickSort2App
+    {
+    public static void main(String[] args)
+        {
+        int maxSize = 16; // array size
+        ArrayIns arr; // reference to array
+        arr = new ArrayIns(maxSize); // create the array
+        
+        for(int j=0; j<maxSize; j++) // fill array with
+            { // random numbers
+            long n = (int)(java.lang.Math.random()*99);
+            arr.insert(n);
+            }
+            
+        arr.display(); // display items
+        arr.quickSort(); // quicksort them
+        arr.display(); // display them again
+        } // end main()
+    } // end class QuickSort2App
