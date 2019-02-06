3f29cc00acf59278f51bbde4aee2a445f4a1c5c4
David Jiang <djiang86@binghamton.edu>
2019-02-06 04:43:25 +0000

    solution 1 smallest difference

---
+++ Redo3/Moderate/SmallestDifference/Solution1.java
@@ -0,0 +1,19 @@
+package Moderate.SmallestDifference;
+
+public class Solution1 {
+    public int findSmallestDifference(int[] array1, int[] array2) {
+        if (array1.length == 0 || array2.length == 0) return -1;
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (Math.abs(array1[i] - array2[j]) < min) {
+                    min = Math.abs(array1[i] - array2[j]);
+                }
+            }
+        }
+
+        return min;
+    }
+}
