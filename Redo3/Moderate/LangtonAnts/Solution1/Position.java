45440f444f74462fba4c8d0d4fba3c90c08067d9
David Jiang <djiang86@binghamton.edu>
2019-02-07 03:31:59 +0000

    ant, orientation, position class for langton ants

---
+++ Redo3/Moderate/LangtonAnts/Solution1/Position.java
@@ -0,0 +1,11 @@
+package Moderate.LangtonAnts.Solution1;
+
+public class Position {
+    public int row;
+    public int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+}
