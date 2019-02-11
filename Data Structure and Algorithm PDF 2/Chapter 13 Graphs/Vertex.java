61109f19e651668ed15a9d46d19a92a58e59f405
David Jiang <djiang86@binghamton.edu>
2019-02-11 13:33:32 +0000

    vertex for a graph

---
+++ Data Structure and Algorithm PDF 2/Chapter 13 Graphs/Vertex.java
@@ -0,0 +1,11 @@
+class Vertex
+    {
+    public char label; // label (e.g. ‘A’)
+    public boolean wasVisited;
+    
+    public Vertex(char lab) // constructor
+        {
+        label = lab;
+        wasVisited = false;
+        }
+    } // end class Vertex
