eeea0dbafe39d182b9ed5ac12d6afc03883214f4
David Jiang <djiang86@binghamton.edu>
2019-02-11 13:40:58 +0000

    graph class

---
+++ Data Structure and Algorithm PDF 2/Chapter 13 Graphs/Graph.java
@@ -0,0 +1,36 @@
+class Graph
+    {
+    private final int MAX_VERTS = 20;
+    private Vertex vertexList[]; // array of vertices
+    private int adjMat[][]; // adjacency matrix
+    private int nVerts; // current number of vertices
+    // -------------------------------------------------------------
+    public Graph() // constructor
+        {
+        vertexList = new Vertex[MAX_VERTS];
+        // adjacency matrix
+        adjMat = new int[MAX_VERTS][MAX_VERTS];
+        nVerts = 0;
+        
+        for(int j=0; j<MAX_VERTS; j++) // set adjacency
+            for(int k=0; k<MAX_VERTS; k++) // matrix to 0
+                adjMat[j][k] = 0;
+        } // end constructor
+    // -------------------------------------------------------------
+    public void addVertex(char lab) // argument is label
+        {
+        vertexList[nVerts++] = new Vertex(lab);
+        }
+    // -------------------------------------------------------------
+    public void addEdge(int start, int end)
+        {
+        adjMat[start][end] = 1;
+        adjMat[end][start] = 1;
+        }
+    // -------------------------------------------------------------
+    public void displayVertex(int v)
+        {
+        System.out.print(vertexList[v].label);
+        }
+    // -------------------------------------------------------------
+    } // end class Graph
