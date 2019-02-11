cc4a51b86980516ce59a6574ff35139c4546aeda
David Jiang <djiang86@binghamton.edu>
2019-02-11 12:50:28 +0000

    node for heap

---
+++ Data Structure and Algorithm PDF 2/Chapter 12 Heaps/Node.java
@@ -0,0 +1,14 @@
+class Node
+    {
+    private int iData; // data item (key)
+    // -------------------------------------------------------------
+    public Node(int key) // constructor
+        { iData = key; }
+    // -------------------------------------------------------------
+    public int getKey()
+        { return iData; }
+    // -------------------------------------------------------------
+    public void setKey(int id)
+        { iData = id; }
+    // -------------------------------------------------------------
+    } // end class Node
