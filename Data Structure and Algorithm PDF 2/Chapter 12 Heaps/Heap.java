128c486fb5da0669828fc864117f09cf015bfd70
David Jiang <djiang86@binghamton.edu>
2019-02-11 12:53:39 +0000

    trickle down in heap

--- Data Structure and Algorithm PDF 2/Chapter 12 Heaps/Heap.java
+++ Data Structure and Algorithm PDF 2/Chapter 12 Heaps/Heap.java
@@ -39,4 +39,38 @@ class Heap
         heapArray[index] = bottom;
         } // end trickleUp()
     // -------------------------------------------------------------
+    public Node remove() // delete item with max key
+        { // (assumes non-empty list)
+        Node root = heapArray[0];
+        heapArray[0] = heapArray[--currentSize];
+        trickleDown(0);
+        return root;
+        } // end remove()
+        // -------------------------------------------------------------
+    public void trickleDown(int index)
+        {
+        int largerChild;
+        Node top = heapArray[index]; // save root
+        
+        while(index < currentSize/2) // while node has at
+            { // least one child,
+            int leftChild = 2*index+1;
+            int rightChild = leftChild+1;
+            // find larger child
+            if(rightChild < currentSize && // (rightChild exists?)
+                heapArray[leftChild].getKey() <
+                heapArray[rightChild].getKey())
+                largerChild = rightChild;
+            else
+                largerChild = leftChild;
+                
+            // top >= largerChild?
+            if( top.getKey() >= heapArray[largerChild].getKey() )
+                break;
+            // shift child up
+            heapArray[index] = heapArray[largerChild];
+            index = largerChild; // go down
+            } // end while
+        heapArray[index] = top; // root to index
+        } // end trickleDown()
     }
