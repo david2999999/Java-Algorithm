123e5d380459783b41275dd6024cec839287db14
David Jiang <djiang86@binghamton.edu>
2019-02-12 20:38:12 +0000

    BFS App

---
+++ Data Structure and Algorithm PDF 2/Chapter 13 Graphs/Breadth First Search/BFSApp.java
@@ -0,0 +1,20 @@
+class BFSApp
+    {
+    public static void main(String[] args)
+        {
+        Graph theGraph = new Graph();
+        theGraph.addVertex(‘A’); // 0 (start for dfs)
+        theGraph.addVertex(‘B’); // 1
+        theGraph.addVertex(‘C’); // 2
+        theGraph.addVertex(‘D’); // 3
+        theGraph.addVertex(‘E’); // 4
+        theGraph.addEdge(0, 1); // AB
+        theGraph.addEdge(1, 2); // BC
+        theGraph.addEdge(0, 3); // AD
+        theGraph.addEdge(3, 4); // DE
+        System.out.print(“Visits: “);
+        theGraph.bfs(); // breadth-first search
+        System.out.println();
+        } // end main()
+    } // end class BFSApp
+    ////////////////////////////////////////////////////////////////
