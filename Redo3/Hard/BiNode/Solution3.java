b5240ed1bf529ef408189cfd2cd15e26edc0f231
David Jiang <djiang86@binghamton.edu>
2019-02-14 01:22:21 +0000

    solution 3

---
+++ Redo3/Hard/BiNode/Solution3.java
@@ -0,0 +1,48 @@
+package Hard.BiNode;
+
+public class Solution3 {
+    private BiNode convert(BiNode root) {
+        BiNode head = convertToCircular(root);
+        head.node1.node2 = null;
+        head.node1 = null;
+        return head;
+    }
+
+    private BiNode convertToCircular(BiNode root) {
+        if (root == null) return null;
+
+        BiNode part1 = convertToCircular(root.node1);
+        BiNode part3 = convertToCircular(root.node2);
+
+        if (part1 == null && part3 == null) {
+            root.node1 = root;
+            root.node2 = root;
+            return root;
+        }
+
+        BiNode tail3 = (part3 == null) ? null : part3.node1;
+
+        if (part1 == null) {
+            concat(part3.node1, root);
+        } else {
+            concat(part1.node1, root);
+        }
+
+        if (part3 == null) {
+            concat(root, part1);
+        } else {
+            concat(root, part3);
+        }
+
+        if (part1 != null && part3 != null) {
+            concat(tail3, part1);
+        }
+
+        return part1 == null ? root : part1;
+    }
+
+    private void concat(BiNode x, BiNode y) {
+        x.node2 = y;
+        y.node1 = x;
+    }
+}
