0e95a99ef0243e47450a952afc1b343ec65ebb16
David Jiang <djiang86@binghamton.edu>
2019-02-19 11:50:45 +0000

    accounting for first node when inserting into linked list

--- Data Structure PDF/Chapter 3 Linked Data Structure/Sorted Linked List/LinkedList.java
+++ Data Structure PDF/Chapter 3 Linked Data Structure/Sorted Linked List/LinkedList.java
@@ -1,16 +1,22 @@
 public class LinkedList {
     
-     void insert(Node start, int x) {
-         // PRECONDITIONS: the list is in ascending order, and x > start.data;
-         // POSTCONDITIONS: the list is in ascending order, and it contains x;
-         Node p = start;
-         while (p.next != null) {
-             if (p.next.data > x) break;
-                p = p.next;
-         }
-         
-         p.next = new Node(x,p.next);
-     }    
+    Node insert(Node start, int x) {
+        // precondition: the list is in ascending order;
+        // postconditions: the list is in ascending order, and it contains x;
+        if (start == null || start.data > x) {
+            start = new Node(x,start);
+            return start;
+        }
+        
+        Node p=start;
+        while (p.next != null) {
+            if (p.next.data > x) break;
+            p = p.next;
+        }
+        
+        p.next = new Node(x,p.next);
+        return start;
+    } 
     
     
 }
