c2d6d7d1b5ba9465aa0fdf9dbd333959044e8ea9
David Jiang <djiang86@binghamton.edu>
2019-02-19 11:55:36 +0000

    delete node from linkedlist

--- Data Structure PDF/Chapter 3 Linked Data Structure/Sorted Linked List/LinkedList.java
+++ Data Structure PDF/Chapter 3 Linked Data Structure/Sorted Linked List/LinkedList.java
@@ -19,4 +19,24 @@ public class LinkedList {
     } 
     
     
+    Node delete(Node start, int x) {
+        // precondition: the list is in ascending order;
+        // postconditions: the list is in ascending order, and if it did
+        // contains x, then the first occurrence of x has been deleted;
+        if (start == null || start.data > x) { // x is not in the list
+            return start;
+        } else if (start.data == x) { // x is the first element in the list
+            return start.next;
+        }
+        
+        for (Node p = start; p.next != null; p = p.next) {
+            if (p.next.data > x) {
+                break; // x is not in the list
+            } else if (p.next.data == x) { // x is in the p.next node
+                p.next = p.next.next; // delete it
+                break;
+            }
+        }
+        return start;
+    }
 }
