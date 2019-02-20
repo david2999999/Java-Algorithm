010bc8d5e16964b8fc257dc057628f71be783d12
David Jiang <djiang86@binghamton.edu>
2019-02-20 12:25:14 +0000

    using type arguments

---
+++ Data Structure PDF/Chapter 4 Java Collection Framework/Type Arguments/Month.java
@@ -0,0 +1,1 @@
+enum Month { JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC }
---
+++ Data Structure PDF/Chapter 4 Java Collection Framework/Type Arguments/Pair.java
@@ -0,0 +1,21 @@
+ class Pair<S, T> {
+     private S first;
+     private T second;
+    
+     public Pair(S first, T second) {
+         this.first = first;
+         this.second = second;
+     }
+    
+     public S getFirst() {
+        return first;
+     }
+    
+     public T getSecond() {
+        return second;
+     }
+    
+     public String toString() {
+        return "(" + first + ", " + second + ")";
+     }
+ }
