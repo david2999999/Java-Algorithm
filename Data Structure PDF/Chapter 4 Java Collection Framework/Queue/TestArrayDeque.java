99a9ab4c613bb91d4e698861d0495f9187fe741d
David Jiang <djiang86@binghamton.edu>
2019-02-25 11:29:09 +0000

    test array deque

---
+++ Data Structure PDF/Chapter 4 Java Collection Framework/Queue/TestArrayDeque.java
@@ -0,0 +1,21 @@
+ public class TestArrayDeque {
+     public static void main(String[] args) {
+         Deque<String> deque = new ArrayDeque<String>();
+         Collections.addAll(deque, "GB", "DE", "FR", "ES");
+         System.out.println(deque);
+         System.out.println("deque.getFirst(): " + deque.getFirst());
+         System.out.println(deque);
+         System.out.println("deque.removeFirst(): " + deque.removeFirst());
+         System.out.println(deque);
+         System.out.println("deque.addFirst(\"IT\"):");
+         deque.addFirst("IT");
+         System.out.println(deque);
+         System.out.println("deque.getLast(): " + deque.getLast());
+         System.out.println(deque);
+         System.out.println("deque.removeLast(): " + deque.removeLast());
+         System.out.println(deque);
+         System.out.println("deque.addLast(\"IE\"):");
+         deque.addLast("IE");
+         System.out.println(deque);
+     }
+ }
