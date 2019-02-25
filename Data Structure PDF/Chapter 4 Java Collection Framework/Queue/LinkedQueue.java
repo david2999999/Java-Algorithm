bacd2d1908dedf0a7cc22e7106a2473c747b80f1
David Jiang <djiang86@binghamton.edu>
2019-02-25 11:16:43 +0000

    linkedqueue extending abstractqueue and implements queue

---
+++ Data Structure PDF/Chapter 4 Java Collection Framework/Queue/LinkedQueue.java
@@ -0,0 +1,31 @@
+ class LinkedQueue<E> extends AbstractQueue<E> implements Queue<E> {
+     private List<E> list = new LinkedList<E>();
+    
+     public Iterator<E> iterator() {
+        return list.iterator();
+     }
+    
+     public boolean offer(E e) {
+         if (e == null) {
+             return false;
+         } else {
+            list.add(e);
+            return true;
+         }
+     }
+     public E peek() {
+        return list.get(0);
+     }
+    
+     public E poll() {
+         if (list.isEmpty()) {
+            return null;
+         } else {
+            return list.remove(0);
+         }
+     }
+    
+     public int size() {
+        return list.size();
+     }
+ }
