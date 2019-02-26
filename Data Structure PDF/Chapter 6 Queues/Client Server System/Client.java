bc76e66929ca2bfd831ccb03fa3f5c3335db2c17
David Jiang <djiang86@binghamton.edu>
2019-02-26 13:04:31 +0000

    started client server system

---
+++ Data Structure PDF/Chapter 6 Queues/Client Server System/Client.java
@@ -0,0 +1,17 @@
+ public class Client {
+     private int id;
+     private int startTime;
+    
+     public Client(int id, int time) {
+         this.id = id;
+         System.out.printf("%s arrived at time %d.%n", this, time);
+     }
+    
+     public void setStartTime(int time) {
+        startTime = time;
+     }
+    
+     public String toString() {
+        return "#" + id;
+     }
+ }
