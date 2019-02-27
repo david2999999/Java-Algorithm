75994c30ef24acfa03919e96e6ba220470856dc7
David Jiang <djiang86@binghamton.edu>
2019-02-27 13:07:02 +0000

    map interface

---
+++ Data Structure PDF/Chapter 8 Hash Tables/Map.java
@@ -0,0 +1,24 @@
+public interface Map {
+    int size();
+    boolean isEmpty();
+    boolean containsKey(Object key);
+    boolean containsValue(Object value);
+    Object get(Object key);
+    Object put(Object key, Object value);
+    Object remove(Object key);
+    void putAll(Map map);
+    void clear();
+    public Set keySet();
+    public Collection values();
+    public Set entrySet();
+    
+    public interface Entry {
+        Object getKey();
+        Object getValue();
+        Object setValue(Object value);
+        boolean equals(Object o);
+        int hashCode();
+    }
+    boolean equals(Object o);
+    int hashCode();
+}
