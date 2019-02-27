770790a06017ff8e828d920b25059e7a696c71d4
David Jiang <djiang86@binghamton.edu>
2019-02-27 13:26:51 +0000

    quadratic probing

---
+++ Data Structure PDF/Chapter 8 Hash Tables/Tests/TestQuadraticProbing.java
@@ -0,0 +1,41 @@
+ public class TestQuadraticProbing {
+     private static final int MASK = 0x7FFFFFFF; // 2^32-1
+     private static final int CAPACITY = 11;
+     private static int size = 0;
+     private static boolean[] used = new boolean[CAPACITY];
+    
+     public static void main(String[] args) {
+         printHash("Rad");
+         printHash("Uhr");
+         printHash("Ohr");
+         printHash("Tor");
+         printHash("Hut");
+         printHash("Tag");
+         printHash("Eis");
+         printHash("Ast");
+         printHash("Zug");
+         printHash("Hof");
+         printHash("Mal");
+     }
+    
+     private static void printHash(String word) {
+         System.out.printf("hash(%s) = %d, load = %d%%%n",
+         word, hash(word), 100*size/CAPACITY);
+     }
+    
+     private static int hash(Object object) {
+         ++size;
+         int h = (object.hashCode() & MASK) % CAPACITY;
+         if (used[h]) {
+             int h0 = h;
+             int jump = 1;
+             while (used[h]) {
+                 System.out.printf("%d, ", h);
+                 h = (h0 + jump*jump)%CAPACITY; // squared increment
+                 ++jump;
+             }
+         }
+         used[h] = true;
+         return h;
+     }
+ }
