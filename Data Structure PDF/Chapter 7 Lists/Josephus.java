32e73754ca5dcfde448413022e56692e6dd0058c
David Jiang <djiang86@binghamton.edu>
2019-02-27 12:34:31 +0000

    josephus

---
+++ Data Structure PDF/Chapter 7 Lists/Josephus.java
@@ -0,0 +1,22 @@
+ public class Josephus {
+     public static final int SOLDIERS = 8;
+     public static final String ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+     public static void main(String[] args) {
+         Ring<String> ring = new Ring<String>();
+         for (int i=0; i<SOLDIERS; i++) {
+            ring.add(ALPHA.substring(i, i+1));
+         }
+         System.out.println(ring);
+         Iterator<String> it = ring.iterator();
+         String killer = it.next();
+         
+         while (ring.size() > 1) {
+             String victim = it.next();
+             System.out.println(killer + " killed " + victim);
+             it.remove();
+             killer = it.next();
+         }
+         System.out.println("The lone survivor is " + it.next());
+     }
+ }
