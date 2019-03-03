13a5d02942e7a2d2848f72fa06e23b88d9a62496
David Jiang <djiang86@binghamton.edu>
2019-03-03 15:28:22 +0000

    deleting node from binary tree with 0 - 1 child nodes

--- Udemy/Trees/BinaryTree/Tree.java
+++ Udemy/Trees/BinaryTree/Tree.java
@@ -21,6 +21,28 @@ public class Tree {
         }
     }
 
+    public void delete(int value) {
+        root = delete(root, value);
+    }
+
+    private TreeNode delete(TreeNode subtreeRoot, int value) {
+        if (subtreeRoot == null) return null;
+
+        if (value < subtreeRoot.getData()) {
+            subtreeRoot.setLeftChild(delete(subtreeRoot.getLeftChild(), value));
+        } else if (value > subtreeRoot.getData()) {
+            subtreeRoot.setRightChild(delete(subtreeRoot.getRightChild(), value));
+        } else {
+            if (subtreeRoot.getLeftChild() == null) {
+                return subtreeRoot.getRightChild();
+            } else if (subtreeRoot.getRightChild() == null) {
+                return subtreeRoot.getLeftChild();
+            }
+        }
+
+        return subtreeRoot;
+    }
+
     public void traverseInOrder() {
         if (root != null) {
             root.traverseInOrder();
