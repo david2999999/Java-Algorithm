b4ebb48dabaaed8dfdeef5652c46e5775d670611
David Jiang <djiang86@binghamton.edu>
2019-03-09 21:22:30 -0500

    Deleting item off heap based on index

--- Udemy/Heap/Heap.java
+++ Udemy/Heap/Heap.java
@@ -17,6 +17,26 @@ public class Heap {
         fixHeapAbove(size++);
     }
 
+    public int delete(int index) {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+
+        int parent = getParent(index);
+        int deletedValue = heap[index];
+
+        heap[index] = heap[size - 1];
+
+        if (index == 0 || heap[index] < heap[parent]) {
+            fixHeapBelow(index, size - 1);
+        } else {
+            fixHeapAbove(index);
+        }
+
+        size--;
+        return deletedValue;
+    }
+
     private void fixHeapAbove(int index) {
         int newValue = heap[index];
         while (index > 0 && newValue > heap[getParent(index)]) {
@@ -27,11 +47,52 @@ public class Heap {
         heap[index] = newValue;
     }
 
+    private void fixHeapBelow(int index, int lastHeapIndex) {
+        int childToSwap;
+        while (index <= lastHeapIndex) {
+            int leftChild = getChild(index, true);
+            int rightChild = getChild(index, false);
+            if (leftChild <= lastHeapIndex) {
+                if (rightChild > lastHeapIndex) {
+                    childToSwap = leftChild;
+                } else {
+                    childToSwap = (heap[leftChild] > heap[rightChild] ? leftChild : rightChild);
+                }
+
+                if (heap[index] < heap[childToSwap]) {
+                    int temp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = temp;
+                } else {
+                    break;
+                }
+
+                index = childToSwap;
+            }
+        }
+    }
+
+    public void printHeap() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(heap[i]);
+            System.out.print(", ");
+        }
+        System.out.println();
+    }
+
     public boolean isFull() {
         return size == heap.length;
     }
 
     public int getParent(int index) {
         return (index - 1) / 2;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getChild(int index, boolean left) {
+        return 2 * index + (left ? 1 : 2);
     }
 }
