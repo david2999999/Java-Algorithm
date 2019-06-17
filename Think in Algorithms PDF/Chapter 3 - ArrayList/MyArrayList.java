888abad185c82d39c0c57afea4f6997189bdafce
david <djiang86@binghamton.edu>
2019-06-17 12:16:53 +0000

    arraylist set implementation

--- Think in Algorithms PDF/Chapter 3 - ArrayList/MyArrayList.java
+++ Think in Algorithms PDF/Chapter 3 - ArrayList/MyArrayList.java
@@ -9,5 +9,10 @@ public class MyArrayList implements List<E> {
         return array[index];
     }
     
+    public E set(int index, E element) {
+        E old = get(index);
+        array[index] = element;
+        return old;
+    }
     
 }
