b2e83d50c375b7e7490907fe7b99d9ba94333214
David2999999 <djiang86@binghamton.edu>
2019-09-24 11:26:39 -0400

    simple list element for singly linked list

---
+++ Programming Interviews Exposed/Chapter 5 - Linked Lists/ListElement.java
@@ -0,0 +1,13 @@
+// A templated Java singly linked list
+public class ListElement<T> {
+    private ListElement<T> next;
+    private T data;
+
+    public ListElement( T value ) { data = value; }
+
+    public ListElement<T> next() { return next; }
+    public T value() { return data; }
+
+    public void setNext( ListElement<T> elem ) { next = elem; }
+    public void setValue( T value ) { data = value; }
+}
