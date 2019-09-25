0cef2aa04f85467e50dca85a8d09aae7a9c09d43
David2999999 <djiang86@binghamton.edu>
2019-09-25 14:20:54 -0400

    3 kinds of traversals

--- Programming Interviews Exposed/Chapter 6 - Trees and Graph/Traversal.java
+++ Programming Interviews Exposed/Chapter 6 - Trees and Graph/Traversal.java
@@ -6,4 +6,20 @@ public class Traversal {
         preorderTraversal( root.getLeft() );
         preorderTraversal( root.getRight() );
     }
+
+    void inorderTraversal( Node root ){
+        if ( root == null ) return;
+
+        inorderTraversal( root.getLeft() );
+        root.printValue();
+        inorderTraversal( root.getRight() );
+    }
+
+    void postorderTraversal( Node root ){
+        if ( root == null ) return;
+
+        postorderTraversal( root.getLeft() );
+        postorderTraversal( root.getRight() );
+        root.printValue();
+    }
 }
