3864dee6809a64aaadcbd592e36295423b277336
David2999999 <djiang86@binghamton.edu>
2019-09-26 10:39:54 -0400

    efficient lookup of kevin bacon number

--- Programming Interviews Exposed/Chapter 6 - Trees and Graph/KevinBacon.java
+++ Programming Interviews Exposed/Chapter 6 - Trees and Graph/KevinBacon.java
@@ -1,5 +1,35 @@
 public class KevinBacon {
     private static final String KEVIN_BACON = "KEVIN BACON";
+    private Map<String, ActorGraphNode> actors = new HashMap<>();
+
+    public int findKevinBaconNumber(ActorGraphNode actor) {
+        if (actor == null || !actors.containsKey(actor.name)) return -1;
+
+        return actors.get(actor.name).baconNumber;
+    }
+
+    public void setBaconNumbers(ActorGraphNode kevinBaconNode) {
+        if (kevinBaconNode == null) return;
+
+        kevinBaconNode.baconNumber = 0;
+        actors.put(kevinBaconNode.name, kevinBaconNode);
+
+        Queue<ActorGraphNode> queue = new LinkedList<>();
+        queue.add(kevinBaconNode);
+
+        ActorGraphNode currentActor;
+        while (!queue.isEmpty()) {
+            currentActor = queue.poll();
+
+            for (ActorGraphNode adjacentActor: currentActor.adjacentActors) {
+                if (adjacentActor.baconNumber != -1) {
+                    adjacentActor.baconNumber = currentActor.baconNumber + 1;
+                    actors.put(adjacentActor.name, adjacentActor);
+                    queue.add(adjacentActor);
+                }
+            }
+        }
+    }
 
     public int findKevinBaconNumberForOneActor(ActorGraphNode actor) {
         if (actor == null) return -1;
