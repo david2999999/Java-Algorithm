6da3156950d22ad368602b6209aa3b65d829927e
David2999999 <djiang86@binghamton.edu>
2019-09-26 11:16:09 -0400

    finding the shortest path with all the names to kevin bacon

--- Programming Interviews Exposed/Chapter 6 - Trees and Graph/KevinBacon.java
+++ Programming Interviews Exposed/Chapter 6 - Trees and Graph/KevinBacon.java
@@ -1,11 +1,45 @@
 public class KevinBacon {
     private static final String KEVIN_BACON = "KEVIN BACON";
     private Map<String, ActorGraphNode> actors = new HashMap<>();
+    private HashMapList<String, String> pathsFromKevinBacon = new HashMapList<>();
 
     public int findKevinBaconNumber(ActorGraphNode actor) {
         if (actor == null || !actors.containsKey(actor.name)) return -1;
 
         return actors.get(actor.name).baconNumber;
+    }
+
+    public ArrayList<String> findingAllNamesToKevinBacon(ActorGraphNode actor) {
+        if (actor == null || !pathsFromKevinBacon.containsKey(actor.name)) return null;
+
+        return pathsFromKevinBacon.get(actor.name);
+    }
+
+    public void findShortestPathFromKevinBacon(ActorGraphNode kevinBaconNode) {
+        if (kevinBaconNode == null) return;
+
+        pathsFromKevinBacon.put(kevinBaconNode.name, kevinBaconNode.name);
+
+        kevinBaconNode.baconNumber = 0;
+        Queue<ActorGraphNode> queue = new LinkedList<ActorGraphNode>();
+        queue.add(kevinBaconNode);
+
+        ActorGraphNode current;
+        while (!queue.isEmpty()) {
+            current = queue.poll();
+
+            for (ActorGraphNode adjacentActor: current.adjacentActors) {
+                if (adjacentActor.baconNumber != -1) {
+                    adjacentActor.baconNumber = current.baconNumber + 1;
+
+                    @SuppressWarnings("unchecked")
+                    ArrayList<String> newPath = (ArrayList<String>) pathsFromKevinBacon.get(current.name).clone();
+                    newPath.add(adjacentActor.name);
+
+                    pathsFromKevinBacon.put(adjacentActor.name, newPath);
+                }
+            }
+        }
     }
 
     public void setBaconNumbers(ActorGraphNode kevinBaconNode) {
