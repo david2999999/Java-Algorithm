ddac20cb8888a5e74b8283b3a3508e623947a50e
David2999999 <djiang86@binghamton.edu>
2019-09-27 10:40:52 -0400

    converting integer to string

---
+++ Programming Interviews Exposed/Chapter 7 - Arrays and Strings/IntegerToString.java
@@ -0,0 +1,33 @@
+public class IntegerToString {
+    public static final int MAX_DIGITS = 10;
+
+    public static void main(String[] args) {
+        System.out.println(convert(-1231412));
+    }
+
+    public static String convert(int num) {
+        boolean isNegative = false;
+
+        if (num < 0) {
+            isNegative = true;
+            num = -num;
+        }
+
+        int currentIndex = 0;
+        char[] temp = new char[MAX_DIGITS + 1];
+
+        while (num != 0) {
+            temp[currentIndex++] = (char) ((num % 10) + '0');
+            num /= 10;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if (isNegative) sb.append("-");
+
+        while(currentIndex > 0) {
+            sb.append(temp[--currentIndex]);
+        }
+
+        return sb.toString();
+    }
+}
