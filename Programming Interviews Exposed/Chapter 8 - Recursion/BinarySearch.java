382c7bc155066cc29bccaec8e7b678258b6a2841
David2999999 <djiang86@binghamton.edu>
2019-09-27 14:06:48 -0400

    binary search iteratively

--- Programming Interviews Exposed/Chapter 8 - Recursion/BinarySearch.java
+++ Programming Interviews Exposed/Chapter 8 - Recursion/BinarySearch.java
@@ -3,10 +3,14 @@ public class BinarySearch {
         return binarySearchRecursively(array, target, 0, array.length - 1);
     }
 
+    public int search2(int[] array, int target) {
+        return binarySearchIteratively(array, target);
+    }
+
     private int binarySearchRecursively(int[] array, int target, int lower, int upper) {
         if (lower > upper) return -1;
 
-        int mid = (upper - lower) / 2;
+        int mid = (upper + lower) / 2;
 
         if (array[mid] == target) {
             return mid;
@@ -14,6 +18,24 @@ public class BinarySearch {
             return binarySearchRecursively(array, target, mid + 1, upper);
         } else {
             return binarySearchRecursively(array, target, lower, mid - 1);
+        }
+    }
+
+    private int binarySearchIteratively(int[] array, int target) {
+        int lower = 0, upper = array.length - 1;
+        int mid;
+
+        while(true) {
+            if (lower > upper) return -1;
+
+            mid = (upper + lower) / 2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                lower = mid + 1;
+            } else {
+                upper = mid - 1;
+            }
         }
     }
 }
