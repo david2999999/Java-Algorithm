eff311c5da53bc87117a6a328a19a3dbb84c9f32
David2999999 <djiang86@binghamton.edu>
2019-09-27 09:05:45 -0400

    in replace string reversal

--- Programming Interviews Exposed/Chapter 7 - Arrays and Strings/ReverseWord.java
+++ Programming Interviews Exposed/Chapter 7 - Arrays and Strings/ReverseWord.java
@@ -1,7 +1,45 @@
 public class ReverseWord {
     public static void main(String[] args) {
         String sentence = "Do or do not, there is no try.";
-        System.out.print(reverseWord(sentence));
+        System.out.println(reverseWord(sentence));
+
+        char[] str = {
+                'i','n',' ','s','e','a','r','c','h',' ','o','f',' ','a','l','g','o','r','i','t','h','m','i','c',' ','e','l','e','g','a','n','c','e'
+        };
+
+        reverseWordEfficiently(str);
+        for (char c: str) {
+            System.out.print(c);
+        }
+    }
+
+    public static void reverseWordEfficiently(char[] str) {
+        reverseString(str, 0, str.length - 1);
+
+        int currentPos = 0, start = 0;
+        while (currentPos < str.length) {
+            if (str[currentPos] != ' ') {
+                start = currentPos;
+
+                while (currentPos < str.length && str[currentPos] != ' ')
+                    currentPos++;
+
+                reverseString(str, start, --currentPos);
+            }
+
+            currentPos++;
+        }
+    }
+
+    private static void reverseString(char[] str, int start, int end) {
+        char temp;
+        while (start < end) {
+            temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+
+            start++; end--;
+        }
     }
 
     public static String reverseWord(String str) {
