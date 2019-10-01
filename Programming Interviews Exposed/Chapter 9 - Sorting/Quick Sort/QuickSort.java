f37b6b9ab05ffb33b04b551f68e2528de1c9f1f3
David2999999 <djiang86@binghamton.edu>
2019-10-01 14:10:44 -0400

    quicksort

---
+++ Programming Interviews Exposed/Chapter 9 - Sorting/Quick Sort/QuickSort.java
@@ -0,0 +1,30 @@
+public class QuickSort {
+    public void sort(int arr[], int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
+    }
+
+    private int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[j] = temp;
+
+        return i + 1;
+    }
+}
