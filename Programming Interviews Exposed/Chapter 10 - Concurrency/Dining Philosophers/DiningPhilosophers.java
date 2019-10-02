cb0198e9bbb32a4844b9a075c2e3fb51c6065806
David2999999 <djiang86@binghamton.edu>
2019-10-02 10:32:27 -0400

    dining philospher

---
+++ Programming Interviews Exposed/Chapter 10 - Concurrency/Dining Philosophers/DiningPhilosophers.java
@@ -0,0 +1,71 @@
+public class DiningPhilosophers {
+    // Each "fork" is just an Object we synchronize on
+    private Object[] forks;
+    private Philosopher[] philosophers;
+    // Prepare the forks and philosophers
+    private DiningPhilosophers( int num ){
+        forks = new Object[ num ];
+        philosophers = new Philosopher[ num ];
+        for ( int i = 0; i < num; ++i ){
+            forks[i] = new Object();
+            int fork1 = i;
+            int fork2 = ( i + 1 ) % num;
+            if ( ( i % 2 ) == 0 ){
+                philosophers[i] = new Philosopher( i, fork2, fork1 );
+            } else {
+                philosophers[i] = new Philosopher( i, fork1, fork2 );
+            }
+        }
+    }
+    // Start the eating process
+    public void startEating() throws InterruptedException {
+        for ( int i = 0; i < philosophers.length; ++i ){
+            philosophers[i].start();
+        }
+        // Suspend the main thread until the first philosopher
+        // stops eating, which will never happen — this keeps
+        // the simulation running indefinitely
+        philosophers[0].join();
+    }
+
+    // Each philosopher runs in its own thread.
+    private class Philosopher extends Thread {
+        private int id;
+        private int fork1;
+        private int fork2;
+
+        Philosopher( int id, int fork1, int fork2 ){
+            this.id = id;
+            this.fork1 = fork1;
+            this.fork2 = fork2;
+        }
+
+        public void run() {
+            status( "Ready to eat using forks " + fork1 +
+                    " and " + fork2 );
+            while ( true ){
+                status( "Picking up fork " + fork1 );
+                synchronized( forks[ fork1 ] ){
+                    status( "Picking up fork " + fork2 );
+                    synchronized( forks[ fork2 ] ){
+                        status( "Eating" );
+                    }
+                }
+            }
+        }
+
+        private void status( String msg ){
+            System.out.println( "Philosopher " + id +
+                    ": " + msg );
+        }
+    }
+    // Entry point for simulation
+    public static void main( String[] args ){
+        try {
+            DiningPhilosophers d = new DiningPhilosophers( 5 );
+            d.startEating();
+        }
+        catch ( InterruptedException e ){
+        }
+    }
+}
