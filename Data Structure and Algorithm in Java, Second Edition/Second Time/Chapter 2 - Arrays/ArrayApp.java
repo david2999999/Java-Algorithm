e199d0e31c15abb92b93484111b21c064a8cbf08
David2999999 <djiang86@binghamton.edu>
2019-10-03 09:23:48 -0400

    testing array wrapper class

--- Data Structure and Algorithm in Java, Second Edition/Second Time/Chapter 2 - Arrays/ArrayApp.java
+++ Data Structure and Algorithm in Java, Second Edition/Second Time/Chapter 2 - Arrays/ArrayApp.java
@@ -1,48 +1,46 @@
 public class ArrayApp {
     public static void main(String[] args)
     {
-        long[] arr; // reference to array
-        arr = new long[100]; // make array
-        int nElems = 0; // number of items
-        int j; // loop counter
-        long searchKey; // key of item to search for
-
-        arr[0] = 77; // insert 10 items
-        arr[1] = 99;
-        arr[2] = 44;
-        arr[3] = 55;
-        arr[4] = 22;
-        arr[5] = 88;
-        arr[6] = 11;
-        arr[7] = 00;
-        arr[8] = 66;
-        arr[9] = 33;
+        LowArray arr; // reference
+        arr = new LowArray(100); // create LowArray object
+        int nElems = 0; // number of items in array
+        int j; // loop variable
+        arr.setElem(0, 77); // insert 10 items
+        arr.setElem(1, 99);
+        arr.setElem(2, 44);
+        arr.setElem(3, 55);
+        arr.setElem(4, 22);
+        arr.setElem(5, 88);
+        arr.setElem(6, 11);
+        arr.setElem(7, 00);
+        arr.setElem(8, 66);
+        arr.setElem(9, 33);
         nElems = 10; // now 10 items in array
 
         for(j=0; j<nElems; j++) // display items
-            System.out.print(arr[j] + " ");
+            System.out.print(arr.getElem(j) + " ");
         System.out.println("");
 
-        searchKey = 66; // find item with key 66
+        int searchKey = 26; // search for data item
         for(j=0; j<nElems; j++) // for each element,
-            if(arr[j] == searchKey) // found item?
-                break; // yes, exit before end
-
-        if(j == nElems) // at the end?
-            System.out.println("Can’t find " + searchKey); // yes
-        else
-            System.out.println("Found " + searchKey); // no
-
-        searchKey = 55; // delete item with key 55
-        for(j=0; j<nElems; j++) // look for it
-            if(arr[j] == searchKey)
+            if(arr.getElem(j) == searchKey) // found item?
                 break;
-        for(int k=j; k<nElems-1; k++) // move higher ones down
-            arr[k] = arr[k+1];
-        nElems--; // decrement size
+        if(j == nElems) // no
+            System.out.println("Can’t find " + searchKey);
+        else // yes
+            System.out.println("Found " + searchKey);
+
+        // delete value 55
+        for(j=0; j<nElems; j++) // look for it
+            if(arr.getElem(j) == 55)
+                break;
+
+        for(int k=j; k<nElems; k++) // higher ones down
+            arr.setElem(k, arr.getElem(k+1) );
+            nElems--; // decrement size
 
         for(j=0; j<nElems; j++) // display items
-            System.out.print( arr[j] + " ");
+            System.out.print( arr.getElem(j) + " ");
         System.out.println("");
     }
 }
