7faa1b2c82e009623d9fbddfa349b193546b9f58
david2999999 <djiang86@binghamton.edu>
2019-10-03 19:46:17 -0400

    simple remove node from linked list

--- Cracking the Code Interview/Redo 4/Chapter 2 - Linked Lists/Basics/Node.java
+++ Cracking the Code Interview/Redo 4/Chapter 2 - Linked Lists/Basics/Node.java
@@ -16,4 +16,23 @@ public class Node {
 
         n.next = end;
     }
+
+    public Node deleteNode(Node head, int d) {
+        Node n = head;
+
+        if (n.data == d) {
+            return head.next;
+        }
+
+        while (n.next != null) {
+            if (n.next.data == d) {
+                n.next = n.next.next;
+                return head;
+            }
+
+            n = n.next;
+        }
+
+        return head;
+    }
 }
