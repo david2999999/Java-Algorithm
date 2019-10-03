940447004d4aa2989815db9aabc83d00c1ee02b1
David2999999 <djiang86@binghamton.edu>
2019-10-03 09:21:42 -0400

    creating an array wrapper class

--- Data Structure and Algorithm in Java, Second Edition/Second Time/Chapter 2 - Arrays/Array.java
+++ Data Structure and Algorithm in Java, Second Edition/Second Time/Chapter 2 - Arrays/Array.java
@@ -1,48 +1,13 @@
 public class Array {
-    public static void main(String[] args)
-    {
-        long[] arr; // reference to array
-        arr = new long[100]; // make array
-        int nElems = 0; // number of items
-        int j; // loop counter
-        long searchKey; // key of item to search for
-
-        arr[0] = 77; // insert 10 items
-        arr[1] = 99;
-        arr[2] = 44;
-        arr[3] = 55;
-        arr[4] = 22;
-        arr[5] = 88;
-        arr[6] = 11;
-        arr[7] = 00;
-        arr[8] = 66;
-        arr[9] = 33;
-        nElems = 10; // now 10 items in array
-
-        for(j=0; j<nElems; j++) // display items
-            System.out.print(arr[j] + " ");
-        System.out.println("");
-
-        searchKey = 66; // find item with key 66
-        for(j=0; j<nElems; j++) // for each element,
-            if(arr[j] == searchKey) // found item?
-                break; // yes, exit before end
-
-        if(j == nElems) // at the end?
-            System.out.println("Can’t find " + searchKey); // yes
-        else
-            System.out.println("Found " + searchKey); // no
-
-        searchKey = 55; // delete item with key 55
-        for(j=0; j<nElems; j++) // look for it
-            if(arr[j] == searchKey)
-                break;
-        for(int k=j; k<nElems-1; k++) // move higher ones down
-            arr[k] = arr[k+1];
-        nElems--; // decrement size
-
-        for(j=0; j<nElems; j++) // display items
-            System.out.print( arr[j] + " ");
-        System.out.println("");
-    }
+    private long[] a; // ref to array a
+    //--------------------------------------------------------------
+    public LowArray(int size) // constructor
+    { a = new long[size]; } // create array
+    //--------------------------------------------------------------
+    public void setElem(int index, long value) // set value
+    { a[index] = value; }
+    //--------------------------------------------------------------
+    public long getElem(int index) // get value
+    { return a[index]; }
+//--------------------------------------------------------------
 }
