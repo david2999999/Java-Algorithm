240507df1376d351f2229f04c6c8b0e778ad4e47
David2999999 <djiang86@binghamton.edu>
2019-10-04 07:51:33 -0400

    ordered array

--- Data Structure and Algorithm in Java, Second Edition/Second Time/Chapter 2 - Arrays/Array Basics/Ordered Array/OrderedArray.java
+++ Data Structure and Algorithm in Java, Second Edition/Second Time/Chapter 2 - Arrays/Array Basics/Ordered Array/OrderedArray.java
@@ -1,4 +1,16 @@
 public class OrderedArray {
+    private long[] a; // ref to array a
+    private int nElems; // number of data items
+
+    public OrdArray(int max) // constructor
+    {
+        a = new long[max]; // create array
+        nElems = 0;
+    }
+
+    public int size()
+    { return nElems; }
+
     public int find(long searchKey)
     {
         int lowerBound = 0;
@@ -21,4 +33,37 @@ public class OrderedArray {
             } // end else divide range
         } // end while
     } // end find()
+
+    public void insert(long value) // put element into array
+    {
+        int j;
+        for(j=0; j<nElems; j++) // find where it goes
+            if(a[j] > value) // (linear search)
+                break;
+        for(int k=nElems; k>j; k--) // move bigger ones up
+            a[k] = a[k-1];
+        a[j] = value; // insert it
+        nElems++; // increment size
+    } // end insert()
+
+    public boolean delete(long value)
+    {
+        int j = find(value);
+        if(j==nElems) // can’t find it
+            return false;
+        else // found it
+        {
+            for(int k=j; k<nElems; k++) // move bigger ones down
+                a[k] = a[k+1];
+            nElems--; // decrement size
+            return true;
+        }
+    } // end delete()
+
+    public void display() // displays array contents
+    {
+        for(int j=0; j<nElems; j++) // for each element,
+            System.out.print(a[j] + " "); // display it
+        System.out.println("");
+    }
 }
