0836174c89d5053bcf096102754579c5859c7d03
David Jiang <djiang86@binghamton.edu>
2019-10-04 21:13:42 -0400

    quick sort

---
+++ Cracking the Code Interview/Redo 4/Chapter 10 - Sorting and Searching/Quick Sort/QuickSort.java
@@ -0,0 +1,29 @@
+public class QuickSort {
+    public void quickSort(int[] arr, int left, int right) {
+        int index = partition(arr, left, right);
+
+        if (left < index - 1) {
+            quickSort(arr, left, index - 1);
+        }
+
+        if (index < right) {
+            quickSort(arr, index, right);
+        }
+    }
+
+    private int partition(int[] arr, int left, int right) {
+        int pivot = arr[(left + right) / 2];
+        while (left <= right) {
+            while (arr[left] < pivot) left++;
+            while (arr[right] > pivot) right--;
+
+            if (left <= right) {
+                swap(arr, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        return left;
+    }
+}
