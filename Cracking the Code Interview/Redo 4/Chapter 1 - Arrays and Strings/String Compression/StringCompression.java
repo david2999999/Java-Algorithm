39bb78e498e2e712da8f96ad01cedae2d9a295c7
David Jiang <djiang86@binghamton.edu>
2019-10-05 19:21:11 -0400

    string compression

---
+++ Cracking the Code Interview/Redo 4/Chapter 1 - Arrays and Strings/String Compression/StringCompression.java
@@ -0,0 +1,18 @@
+public class StringCompression {
+    public String compress(String str) {
+        StringBuilder compressed = new StringBuilder();
+        int countConsecutive = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            countConsecutive++;
+
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+                compressed.append(str.charAt(i));
+                compressed.append(countConsecutive);
+                countConsecutive = 0;
+            }
+        }
+
+        return compressed.toString();
+    }
+}
