a1778a019f2ae927c83f5fb21dc5ed503504d128
David Jiang <djiang86@binghamton.edu>
2019-10-05 15:50:43 -0400

    palindrome permutation with less space

---
+++ Cracking the Code Interview/Redo 4/Chapter 1 - Arrays and Strings/Palindrome Permutation/PalindromePemutation2.java
@@ -0,0 +1,21 @@
+public class PalindromePemutation2 {
+    public boolean isPermutationOfPalindrome(String phrase) {
+        int countOdd = 0;
+        int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+
+        for (char c: phrase.toCharArray()) {
+            int x = getCharNumber(c);
+
+            if (x != -1) {
+                table[x]++;
+                if (table[x] % 2 == 1) {
+                    countOdd++;
+                } else {
+                    countOdd--;
+                }
+            }
+        }
+
+        return countOdd <= 1;
+    }
+}
