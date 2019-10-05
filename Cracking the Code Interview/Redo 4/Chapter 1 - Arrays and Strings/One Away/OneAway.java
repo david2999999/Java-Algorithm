a8a8e2c68e1a51063b38c489664a296116e4357b
David Jiang <djiang86@binghamton.edu>
2019-10-05 17:00:09 -0400

    one edit away

---
+++ Cracking the Code Interview/Redo 4/Chapter 1 - Arrays and Strings/One Away/OneAway.java
@@ -0,0 +1,45 @@
+public class OneAway {
+    public boolean oneEditAway(String first, String second) {
+        if (first.length() == second.length()) {
+            return oneEditReplace(first, second);
+        } else if (first.length() + 1 == second.length()) {
+            return oneEditInsert(first, second);
+        } else if (first.length() - 1 == second.length()) {
+            return oneEditInsert(second, first);
+        }
+
+        return false;
+    }
+
+    private boolean oneEditInsert(String s1, String s2) {
+        int index1 = 0;
+        int index2 = 0;
+
+        while (index1 < s1.length() && index2 < s2.length()) {
+            if (s1.charAt(index1) != s2.charAt(index2)) {
+                if (index1 != index2) return false;
+
+                index2++;
+            } else {
+                index1++;
+                index2++;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean oneEditReplace(String first, String second) {
+        boolean foundDifference = false;
+
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) != second.charAt(i)) {
+                if (foundDifference) return false;
+
+                foundDifference = true;
+            }
+        }
+
+        return true;
+    }
+}
