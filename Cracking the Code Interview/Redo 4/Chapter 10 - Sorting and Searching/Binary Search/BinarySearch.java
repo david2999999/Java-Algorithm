05b73aed249aa14a5a502270455f74037e5e45ac
David Jiang <djiang86@binghamton.edu>
2019-10-04 21:37:04 -0400

    binary search iterative

---
+++ Cracking the Code Interview/Redo 4/Chapter 10 - Sorting and Searching/Binary Search/BinarySearch.java
@@ -0,0 +1,21 @@
+public class BinarySearch {
+    public int binarySearch(int[] a, int x) {
+        int low = 0;
+        int high = a.length - 1;
+        int mid;
+
+        while (low <= high) {
+            mid = (low + high) / 2;
+
+            if (a[mid] < x) {
+                low = mid + 1;
+            } else if (a[mid] > x) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return  -1;
+    }
+}
