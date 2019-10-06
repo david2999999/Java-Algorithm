86ed5f2003c9a05c4e65fa50120d82ae787ce08b
David Jiang <djiang86@binghamton.edu>
2019-10-06 16:00:45 -0400

    linkedlist partition optimized

---
+++ Cracking the Code Interview/Redo 4/Chapter 2 - Linked Lists/Partition LinkedList/PartitionLinkedListOptimized.java
@@ -0,0 +1,23 @@
+public class PartitionLinkedList {
+    public LinkedListNode partition(LinkedListNode node, int x) {
+        LinkedListNode head = node;
+        LinkedListNode tail = node;
+
+        while (node != null) {
+            LinkedListNode next = node.next;
+
+            if (node.data < x) {
+                node.next = head;
+                head = node;
+            } else {
+                tail.next = node;
+                tail = node;
+            }
+
+            node = next;
+        }
+
+        tail.next = null;
+        return head;
+    }
+}
