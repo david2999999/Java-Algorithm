0643499491af3f23a690ecf1ee3199d44ede5e8b
David Jiang <djiang86@binghamton.edu>
2019-10-05 21:51:16 -0400

    zero matrix

---
+++ Cracking the Code Interview/Redo 4/Chapter 1 - Arrays and Strings/Zero Matrix/ZeroMatrix.java
@@ -0,0 +1,35 @@
+public class RotateMatrix {
+    public void setZeros(int[][] matrix) {
+        boolean[] row = new boolean[matrix.length];
+        boolean[] column = new boolean[matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    column[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < row.length; i++) {
+            if (row[i]) nullifyRow(matrix, i);
+        }
+
+        for (int j = 0; j < column.length; j++) {
+            if (column[j]) nullifyColumn(matrix, j);
+        }
+    }
+
+    private void nullifyColumn(int[][] matrix, int column) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][column] = 0;
+        }
+    }
+
+    private void nullifyRow(int[][] matrix, int row) {
+        for (int i = 0; i < matrix[0].length; i++) {
+            matrix[row][i] = 0;
+        }
+    }
+}
