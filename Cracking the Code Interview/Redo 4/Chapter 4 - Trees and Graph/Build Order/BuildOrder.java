915fa794992a41b16a6f5781e105c964c1c28047
David Jiang <djiang86@binghamton.edu>
2019-10-09 19:29:10 -0400

    build order of projects with dependencies

--- Cracking the Code Interview/Redo 4/Chapter 4 - Trees and Graph/Build Order/BuildOrder.java
+++ Cracking the Code Interview/Redo 4/Chapter 4 - Trees and Graph/Build Order/BuildOrder.java
@@ -1,6 +1,40 @@
 public class BuildOrder {
     public Project[] findBuilderOrder(String[] projects, String[][] dependencies) {
         Graph graph = buildGraph(projects, dependencies);
+        return orderProjects(graph.getNodes());
+    }
+
+    private Project[] orderProjects(ArrayList<Project> projects) {
+        Project[] order = new Project[projects.size()];
+
+        int endOfList = addNonDependents(order, projects, 0);
+
+        int toBeProcessed = 0;
+        while (toBeProcessed < order.length) {
+            Project current = order[toBeProcessed];
+
+            if (current == null) return null;
+
+            ArrayList<Project> children = current.getChildren();
+            for (Project child: children) {
+                child.decrementDependencies();
+            }
+
+            endOfList = addNonDependents(order, children, endOfList);
+            toBeProcessed++;
+        }
+
+        return order;
+    }
+
+    private int addNonDependents(Project[] order, ArrayList<Project> projects, int offset) {
+        for (Project project: projects) {
+            if (project.getNumberDependencies() == 0) {
+                order[offset++] = project;
+            }
+        }
+
+        return offset;
     }
 
     private Graph buildGraph(String[] projects, String[][] dependencies) {
