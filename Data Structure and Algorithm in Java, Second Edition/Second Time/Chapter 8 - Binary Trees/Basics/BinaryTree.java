325a19315db2510a6bad7530a526fb961af139bf
David2999999 <djiang86@binghamton.edu>
2019-10-17 08:03:58 -0400

    deleting a node that has no children

--- Data Structure and Algorithm in Java, Second Edition/Second Time/Chapter 8 - Binary Trees/Basics/BinaryTree.java
+++ Data Structure and Algorithm in Java, Second Edition/Second Time/Chapter 8 - Binary Trees/Basics/BinaryTree.java
@@ -59,7 +59,33 @@ public class BinaryTree {
         }
     }
 
-    public void delete(int key) {
+    public boolean delete(int key) {
+        Node current = root;
+        Node parent = root;
+        boolean isLeftChild = true;
 
+        while (current.data != key) {
+            parent = current;
+
+            if (key < current.data) {
+                isLeftChild = true;
+                current = current.leftChild;
+            } else {
+                isLeftChild = false;
+                current = current.rightChild;
+            }
+
+            if (current == null) return false;
+        }
+
+        if (current.leftChild == null && current.rightChild == null) {
+            if (current == root) {
+                root = null;
+            } else if (isLeftChild) {
+                parent.leftChild = null;
+            } else {
+                parent.rightChild = null;
+            }
+        }
     }
 }
