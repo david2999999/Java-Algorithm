4a85b84659d59db657229339a668f6570406218a
David2999999 <djiang86@binghamton.edu>
2019-10-23 08:15:35 -0400

    insert and trickle up in heap

---
+++ Data Structure and Algorithm in Java, Second Edition/Second Time/Chapter 12 - Heaps/Heap.java
@@ -0,0 +1,37 @@
+public class Heap {
+    private Node[] heapArray;
+    private int maxSize;
+    private int currentSize;
+
+    public Heap(int maxSize) {
+        this.maxSize = maxSize;
+        currentSize = 0;
+        heapArray = new Node[maxSize];
+    }
+
+    public boolean isEmpty() {
+        return currentSize == 0;
+    }
+
+    public boolean insert(int key) {
+        if (currentSize == maxSize) return false;
+
+        Node newNode = new Node(key);
+        heapArray[currentSize] = newNode;
+        trickleUp(currentSize++);
+        return true;
+    }
+
+    public void trickleUp(int index) {
+        int parent = (index - 1) / 2;
+        Node bottom = heapArray[index];
+
+        while (index > 0 && heapArray[parent].getKey() < bottom.getKey()) {
+            heapArray[index] = heapArray[parent];
+            index = parent;
+            parent = (parent - 1) / 2;
+        }
+
+        heapArray[index] = bottom;
+    }
+}
---
+++ Data Structure and Algorithm in Java, Second Edition/Second Time/Chapter 12 - Heaps/Node.java
@@ -0,0 +1,15 @@
+public class Node {
+    private int data;
+
+    public Node (int data) {
+        this.data = data;
+    }
+
+    public int getKey() {
+        return data;
+    }
+
+    public void setKey(int data) {
+        this.data = data;
+    }
+}
