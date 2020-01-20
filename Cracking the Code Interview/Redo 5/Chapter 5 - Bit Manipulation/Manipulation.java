b53f41905e36764544498aa969c07950f1fee01f
david2999999 <djiang86@binghamton.edu>
2020-01-20 08:08:33 -0500

    setting bit at position i

--- Cracking the Code Interview/Redo 5/Chapter 5 - Bit Manipulation/Manipulation.java
+++ Cracking the Code Interview/Redo 5/Chapter 5 - Bit Manipulation/Manipulation.java
@@ -2,4 +2,8 @@ public class Manipulation {
     public boolean getBit(int num, int i) {
         return ((num & (1 << i)) != 0);
     }
+
+    public int setBit(int num, int i) {
+        return num | (i << i);
+    }
 }
