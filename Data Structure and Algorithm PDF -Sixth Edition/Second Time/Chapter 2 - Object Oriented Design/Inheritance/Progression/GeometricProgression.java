6ce31f2d7fdb794be02339fdaaa079d5bda085c3
David Jiang <djiang86@Binghamton.edu>
2020-01-26 11:07:01 -0500

    inheritance with geometric progression

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 2 - Object Oriented Design/Inheritance/Progression/GeometricProgression.java
@@ -0,0 +1,20 @@
+public class GeometricProgression extends Progression {
+    protected long base;
+
+    public GeometricProgression() {
+        this(2, 1);
+    }
+
+    public GeometricProgression(long b) {
+        this(b, 1);
+    }
+
+    public GeometricProgression(long b, long start) {
+        super(start);
+        base = b;
+    }
+
+    protected void advance() {
+        cureent *= base;
+    }
+}
