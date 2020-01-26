d70c4a0bc379909169df44858197d1f4d102ee52
David Jiang <djiang86@Binghamton.edu>
2020-01-26 14:19:59 -0500

    classic style of representing a generic pair

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 2 - Object Oriented Design/Casting and Generics/Generic/ObjectPair.java
@@ -0,0 +1,17 @@
+public class ObjectPair {
+    Object first;
+    Object second;
+
+    public ObjectPair(Object a, Object b) {
+        this.first = a;
+        this.second = b;
+    }
+
+    public Object getFirst() {
+        return first;
+    }
+
+    public Object getSecond() {
+        return second;
+    }
+}
