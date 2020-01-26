ac4203ef54a1890568daf59f32dbe394ba4fe7de
David Jiang <djiang86@Binghamton.edu>
2020-01-26 10:35:11 -0500

    progression parent class

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 2 - Object Oriented Design/Inheritance/Progression/Progression.java
@@ -0,0 +1,29 @@
+public class Progression {
+    protected long current;
+
+    public Progression() {
+        this(0);
+    }
+
+    public Progression(long start) {
+        current = start;
+    }
+
+    public long nextValue() {
+        long answer = current;
+        advance();
+        return answer;
+    }
+
+    public void advance() {
+        current++;
+    }
+
+    public void printProgression(int n) {
+        System.out.println(nextValue());
+        for (int j = 1; j < n; j++) {
+            System.out.println(" " + nextValue());
+        }
+        System.out.println();
+    }
+}
