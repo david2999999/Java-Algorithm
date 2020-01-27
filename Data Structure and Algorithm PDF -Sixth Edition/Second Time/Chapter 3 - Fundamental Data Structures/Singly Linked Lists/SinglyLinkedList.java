cde9023e00d28702ba6d0376772b97c444f62bee
David Jiang <djiang86@Binghamton.edu>
2020-01-26 21:45:15 -0500

    node for singly linked list

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 3 - Fundamental Data Structures/Singly Linked Lists/SinglyLinkedList.java
@@ -0,0 +1,23 @@
+public class SinglyLinkedList<E> {
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+
+        public Node(E e, Node<E> n) {
+            element = e;
+            next = n;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> node) {
+            next = node;
+        }
+    }
+}
