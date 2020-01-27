8b44d35b6e156f1d6cd8e8728dc225d1acf8c1f0
David Jiang <djiang86@Binghamton.edu>
2020-01-26 21:59:26 -0500

    operations of singly linekd list

--- Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 3 - Fundamental Data Structures/Singly Linked Lists/SinglyLinkedList.java
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 3 - Fundamental Data Structures/Singly Linked Lists/SinglyLinkedList.java
@@ -20,4 +20,79 @@ public class SinglyLinkedList<E> {
             next = node;
         }
     }
+
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private int size = 0;
+
+    public SinglyLinkedList() {}
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E first() {
+        if (isEmpty()) return null;
+        return head.getElement();
+    }
+
+    public E last() {
+        if (isEmpty()) return null;
+        return tail.getElement();
+    }
+
+    public void addFirst(E e) {
+        head = new Node<>(e, head);
+
+        if (isEmpty()) {
+            tail = head;
+        }
+
+        size++;
+    }
+
+    public void addLast(E e) {
+        Node<E> newest = new Node<>(e, null);
+        if (isEmpty()) {
+            head = newest;
+        } else {
+            tail.setNext(newest);
+        }
+
+        tail = newest;
+        size++;
+    }
+
+    public E removeFirst() {
+        if (isEmpty()) return null;
+
+        E answer = head.getElement();
+        head = head.getNext();
+        size--;
+
+        if (size == 0) tail = null;
+
+        return answer;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
