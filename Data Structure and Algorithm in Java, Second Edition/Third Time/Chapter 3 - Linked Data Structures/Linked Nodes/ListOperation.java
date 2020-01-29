2a388d12cc7d4c2bd4055437d7d680cb5aeddd2d
david2999999 <djiang86@binghamton.edu>
2020-01-29 09:31:45 -0500

    deleting a node in a sorted linked list

--- Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 3 - Linked Data Structures/Linked Nodes/ListOperation.java
+++ Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 3 - Linked Data Structures/Linked Nodes/ListOperation.java
@@ -13,4 +13,26 @@ public class ListOperation {
         p.next = new Node(x, p.next);
         return start;
     }
+
+    // precondition: the list is in ascending order;
+    // postconditions: the list is in ascending order, and if it did
+    // contains x, then the first occurrence of x has been deleted;
+    public Node delete(Node start, int x) {
+        if (start == null || start.data > x) {
+            return start;
+        } else if (start.data == x) {
+            return start.next;
+        }
+
+        for (Node p = start; p.next != null; p = p.next) {
+            if (p.next.data > x) {
+                break;
+            } else if (p.next.data == x) {
+                p.next = p.next.next;
+                break;
+            }
+        }
+
+        return start;
+    }
 }
