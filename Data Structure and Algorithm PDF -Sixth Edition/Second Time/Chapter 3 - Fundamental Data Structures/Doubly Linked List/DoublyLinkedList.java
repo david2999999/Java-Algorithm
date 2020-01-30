5fd285978ab3fe3faf4d5a9be6f079744457adf7
David Jiang <djiang86@Binghamton.edu>
2020-01-29 21:01:46 -0500

    node for doubly linked list

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 3 - Fundamental Data Structures/Doubly Linked List/DoublyLinkedList.java
@@ -0,0 +1,37 @@
+public class DoublyLinkedList<E> {
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public Node(E e, Node<E> p, Node<E> n) {
+            element = e;
+            prev = p;
+            next = n;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node<E> prev) {
+            this.prev = prev;
+        }
+    }
+}
