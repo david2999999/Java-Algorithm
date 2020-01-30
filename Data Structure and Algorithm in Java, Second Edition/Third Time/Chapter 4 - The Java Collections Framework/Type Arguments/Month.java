e520b0cf564d2f1759b0ed2da94e54be5785dc3d
david2999999 <djiang86@binghamton.edu>
2020-01-30 09:07:47 -0500

    using type arguments with generic

---
+++ Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 4 - The Java Collections Framework/Type Arguments/Month.java
@@ -0,0 +1,3 @@
+public enum Month {
+    JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
+}
---
+++ Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 4 - The Java Collections Framework/Type Arguments/TestPairClass.java
@@ -0,0 +1,9 @@
+public class TestPairClass {
+    public static void main(String[] args) {
+        Pair<Month, Integer> christmas = new Pair<Month, Integer>(Month.DEC, 25);
+        System.out.println(christmas);
+        Month month = christmas.getFirst();
+        int day = christmas.getSecond();
+        System.out.printf("%d %s%n", day, month);
+    }
+}
