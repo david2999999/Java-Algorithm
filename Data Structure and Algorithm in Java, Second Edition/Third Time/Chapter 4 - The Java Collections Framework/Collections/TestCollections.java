c9b7fcfea2cd44a781488d12adbafd424d55b3f8
david2999999 <djiang86@binghamton.edu>
2020-01-31 11:25:40 -0500

    utility methods from collection class

---
+++ Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 4 - The Java Collections Framework/Collections/TestCollections.java
@@ -0,0 +1,19 @@
+public class TestCollections {
+    public static void main(String[] args) {
+        List g8 = new ArrayList();
+        Collections.addAll(g8, "US", "DE", "JP", "FR", "GB", "RU", "CA", "IT");
+        System.out.println(g8);
+        Collections.swap(g8, 2, 4);
+        System.out.println(g8);
+        Collections.sort(g8);
+        System.out.println(g8);
+        int k = Collections.binarySearch(g8, "CN");
+        System.out.println(k);
+        if (k < 0) {
+            g8.add(-k - 1, "CN");
+        }
+        System.out.println(g8);
+        Collections.reverse(g8);
+        System.out.println(g8);
+    }
+}
