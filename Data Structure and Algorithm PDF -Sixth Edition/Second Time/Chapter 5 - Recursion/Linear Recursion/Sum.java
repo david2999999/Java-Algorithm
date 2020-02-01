150fbf009c16a44e1048a26957eb41ec43f21565
David Jiang <djiang86@Binghamton.edu>
2020-02-01 10:46:27 -0500

    recursive linear sum

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 5 - Recursion/Linear Recursion/Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+    public static int linearSum(int[] data, int n) {
+        if (n == 0)
+            return 0;
+        else
+            return linearSum(data, n - 1) + data[n - 1];
+    }
+}
