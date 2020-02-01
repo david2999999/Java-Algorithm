e7d05089fa529e6e69afe70678d08ec6fe014b99
David Jiang <djiang86@Binghamton.edu>
2020-02-01 08:42:06 -0500

    recursion with factorial

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 5 - Recursion/Factorial/Factorial.java
@@ -0,0 +1,11 @@
+public class Factorial {
+    public static int factorial(int n) throws IllegalArgumentException {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        } else if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
