9d06177761c5735fdbc36f2cad7b1b7fdce144e1
David Jiang <djiang86@Binghamton.edu>
2020-02-02 05:09:45 -0500

    recursive calculation of power using squaring technique

--- Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 5 - Recursion/Powers/Powers.java
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 5 - Recursion/Powers/Powers.java
@@ -5,4 +5,19 @@ public class Powers {
         else
             return x * power(x, n - 1);
     }
+
+    public static double power2(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        double partial = power2(x, n / 2);
+        double result = partial * partial;
+
+        if (n % 2 == 1) {
+            result *= x;
+        }
+
+        return result;
+    }
 }
