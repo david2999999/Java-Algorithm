42e40181d1f57959f26c3c626feadd2231dcd06f
David Jiang <djiang86@Binghamton.edu>
2020-02-02 05:26:14 -0500

    recursive binary sum

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 5 - Recursion/Binary Recursion/Binary Sum/BinarySum.java
@@ -0,0 +1,12 @@
+public class BinarySum {
+    public static int sum(int[] data, int low, int high) {
+        if (low > high) {
+            return 0;
+        } else if (low == high) {
+            return data[low];
+        } else {
+            int mid = (low + high) / 2;
+            return sum(data, low, mid) + sum(data, mid + 1, high);
+        }
+    }
+}
