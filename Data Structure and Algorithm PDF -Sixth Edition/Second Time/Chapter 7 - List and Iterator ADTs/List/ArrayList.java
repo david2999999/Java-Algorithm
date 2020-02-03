5fa54ac5a38b9c0754f74cbd797cc58bde93b115
David Jiang <djiang86@Binghamton.edu>
2020-02-03 18:28:37 -0500

    array list implementation

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 7 - List and Iterator ADTs/List/ArrayList.java
@@ -0,0 +1,64 @@
+public class ArrayList<E> implements List<E> {
+    public static final int CAPACITY = 16;
+    private E[] data;
+    private int size = 0;
+
+    public ArrayList() {
+        this(CAPACITY);
+    }
+
+    public ArrayList(int capacity) {
+        data = (E[]) new Object[capacity];
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public E get(int i) throws IndexOutOfBoundsException {
+        checkIndex(i, size);
+        return data[i];
+    }
+
+    public E set(int i, E e) throws IndexOutOfBoundsException {
+        checkIndex(i, size);
+        E temp = data[i];
+        data[i] = e;
+        return temp;
+    }
+
+    public void add(int i, E e) throws IndexOutOfBoundsException, IllegalStateException {
+        checkIndex(i, size + 1);
+        if (size == data.length) {
+            throw new IllegalStateException("Array is full");
+        }
+
+        for (int k = size - 1; k >= i; k--) {
+            data[k + 1] = data[k];
+        }
+
+        data[i] = e;
+        size++;
+    }
+
+    public E remove(int i) throws IndexOutOfBoundsException {
+        checkIndex(i, size);
+        E temp = data[i];
+        for (int k = i; i < size - 1; k++) {
+            data[k] = data[k + 1];
+        }
+
+        data[size - 1] = null;
+        size--;
+        return temp;
+    }
+
+    private void checkIndex(int i, int n) throws IndexOutOfBoundsException {
+        if (i < 0 || i >= n)
+            throw new IndexOutOfBoundsException("Illegal index: " + i);
+    }
+}
