5bbc3c5c7047850f8b37d8fc70e2fef0dff0a439
david2999999 <djiang86@binghamton.edu>
2020-02-03 08:04:10 -0500

    testing JCF queue

---
+++ Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 6 - Queues/Test/TestStringQueue.java
@@ -0,0 +1,20 @@
+public class TestStringQueue {
+    public static void main(String[] args) {
+        Queue<String> queue = new ArrayDeque<String>();
+        queue.add("GB");
+        queue.add("DE");
+        queue.add("FR");
+        queue.add("ES");
+        System.out.println(queue);
+        System.out.println("queue.element(): " + queue.element());
+        System.out.println("queue.remove(): " + queue.remove());
+        System.out.println(queue);
+        System.out.println("queue.remove(): " + queue.remove());
+        System.out.println(queue);
+        System.out.println("queue.add(\"IE\"): ");
+        queue.add("IE");
+        System.out.println(queue);
+        System.out.println("queue.remove(): " + queue.remove());
+        System.out.println(queue);
+    }
+}
