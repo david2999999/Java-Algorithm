a90690e606dd5ae041a93e9949f993b038cf3155
david2999999 <djiang86@binghamton.edu>
2020-02-04 08:31:29 -0500

    client server system

---
+++ Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 6 - Queues/Client-Server System/ExpRandom.java
@@ -0,0 +1,15 @@
+public class ExpRandom extends java.util.Random {
+    private double mean;
+
+    public ExpRandom(double mean) {
+        this.mean = mean;
+    }
+
+    public double nextDouble() {
+        return -mean * Math.log(1 - Math.random());
+    }
+
+    public int nextInt() {
+        return (int) Math.ceil(nextDouble());
+    }
+}
