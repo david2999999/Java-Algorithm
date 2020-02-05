b08a0534a6ab57ca09983b9895c552244e005273
david2999999 <djiang86@binghamton.edu>
2020-02-05 11:30:45 -0500

    binomial coeffcient iterative

--- Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 9 - Recursion/Binomial Coefficients/BinomialCoefficient.java
+++ Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 9 - Recursion/Binomial Coefficients/BinomialCoefficient.java
@@ -3,4 +3,15 @@ public class BinomialCoefficient {
         if (k == 0 || k == n) return 1;
         return coefficient(n - 1, k - 1) + coefficient(n - 1, k);
     }
+
+    public int coeffcientIterative(int n, int k) {
+        if (n < 2 || k == 0 || k == n) return 1;
+
+        int c = 1;
+        for (int j = 1; j <= k; j++) {
+            c = c * (n - j + 1) / j;
+        }
+
+        return c;
+    }
 }
