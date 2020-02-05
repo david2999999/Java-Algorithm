5d7b3fe3d86363d35557680fb638842691886508
david2999999 <djiang86@binghamton.edu>
2020-02-05 13:38:58 -0500

    fibonacci with dynamic programming

---
+++ Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 9 - Recursion/Dynamic Programming/Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    public int fibonacci(int n) {
+        if (n < 2) return n;
+
+        int[] f = new int[n];
+        f[0] = 0;
+        f[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+
+        return f[n - 1] + f[n - 2];
+    }
+}
