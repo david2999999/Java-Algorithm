9de1daaafb525e8f6c549cd142106630344a318c
david2999999 <djiang86@binghamton.edu>
2020-02-05 15:37:53 -0500

    counting number of nodes in tree with memoization

--- Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 11 - Binary Tree/Count Binary Tree/CountBinaryTree.java
+++ Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 11 - Binary Tree/Count Binary Tree/CountBinaryTree.java
@@ -1,13 +1,34 @@
 public class CountBinaryTree {
     public int count(int numOfNodes) {
-        if (nodes == 0 || nodes == 1) return 1;
+        if (numOfNodes == 0 || numOfNodes == 1) return 1;
+
+        System.out.println("Calculating for " + numOfNodes + " nodes.");
 
         int sum = 0;
-
         for (int i = 0; i < numOfNodes; i++) {
-            sum += (count(i) * count(numOfNodes - i - 1));
+            sum += (count(numOfNodes - i - 1) * count(i));
         }
 
+        return sum;
+    }
+
+    public int countEfficient(int numOfNodes) {
+        int[] size = new int[numOfNodes + 1];
+        return count(numOfNodes, size);
+    }
+
+    public int count(int numOfNodes, int[] size) {
+        if (numOfNodes == 0 || numOfNodes == 1) return 1;
+        if (size[numOfNodes] > 0) return size[numOfNodes];
+
+        System.out.println("Calculating " + numOfNodes + " nodes");
+
+        int sum = 0;
+        for (int i = 0; i < numOfNodes; i++) {
+            sum += (count(i, size) * count(numOfNodes - i - 1, size));
+        }
+
+        size[numOfNodes] = sum;
         return sum;
     }
 }
