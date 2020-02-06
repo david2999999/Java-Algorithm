c5194e91d9b76b6f0af3df9e521ead18243facea
david2999999 <djiang86@binghamton.edu>
2020-02-06 11:34:36 -0500

    bubble sort

---
+++ Data Structure and Algorithm in Java, Second Edition/Third Time/Chapter 14 - Sorting/Bubble Sort/BubbleSort.java
@@ -0,0 +1,11 @@
+public class BubbleSort {
+    public static void sort(int[] a) {
+        for (int i = a.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (a[j] > a[j + 1]) {
+                    swap(a, j, j + 1);
+                }
+            }
+        }
+    }
+}
