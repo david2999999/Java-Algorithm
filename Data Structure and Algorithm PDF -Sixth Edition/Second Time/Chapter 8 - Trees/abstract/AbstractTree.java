cacb9f868177d891ee326748d36aed1f38394c26
David Jiang <djiang86@Binghamton.edu>
2020-02-06 18:43:09 -0500

    recursively finding the height of a position

--- Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 8 - Trees/abstract/AbstractTree.java
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 8 - Trees/abstract/AbstractTree.java
@@ -22,4 +22,12 @@ public class AbstractTree<E> implements Tree<E> {
             return 1 + depth(parent(p));
         }
     }
+
+    public int height(Position<E> p) {
+        int h = 0;
+        for (Position<E> c: children(p))
+            h = Math.max(h, 1 + height(c));
+
+        return h;
+    }
 }
