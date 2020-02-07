cfae74e10e7fe570d6e3f224478b39a084c63c88
david2999999 <djiang86@binghamton.edu>
2020-02-07 06:40:52 -0500

    getting depth of binary treenodes iteratively

--- Cracking the Code Interview/Redo 5/Chapter 4 - Trees and Graphs/List of Depths/DepthsOfTree.java
+++ Cracking the Code Interview/Redo 5/Chapter 4 - Trees and Graphs/List of Depths/DepthsOfTree.java
@@ -20,4 +20,25 @@ public class DepthsOfTree {
         getListOfDepths(root.left, lists, level + 1);
         getListOfDepths(root.right, lists, level + 1);
     }
+
+    public ArrayList<LinkedList<TreeNode>> getListOfDepthsIterative(TreeNode root) {
+        ArrayList<LinkedList<TreeNode>> result = new ArrayList<>();
+
+        LinkedList<TreeNode> currentLevel = new LinkedList<>();
+        if (root != null) currentLevel.add(root);
+
+        while (!currentLevel.isEmpty()) {
+            result.add(currentLevel);
+
+            LinkedList<TreeNode> parents = currentLevel;
+            currentLevel = new LinkedList<>();
+
+            for (TreeNode parent: parents) {
+                if (parent.left != null) currentLevel.add(parent.left);
+                if (parent.right != null) currentLevel.add(parent.right);
+            }
+        }
+
+        return result;
+    }
 }
