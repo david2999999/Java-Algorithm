3425c60c8deee515badf943a319f46b32246a8bd
David Jiang <djiang86@Binghamton.edu>
2020-02-08 09:40:18 -0500

    abstract priority queue

--- Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 9 - Priority Queues/Abstract/AbstractPriorityQueue.java
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 9 - Priority Queues/Abstract/AbstractPriorityQueue.java
@@ -24,4 +24,30 @@ public abstract class AbstractPriorityQueue<K, V> implements PriorityQueue<K, V>
             this.value = value;
         }
     }
+
+    private Comparator<K> comp;
+
+    protected AbstractPriorityQueue(Comparator<K> c) {
+        comp = c;
+    }
+
+    protected AbstractPriorityQueue() {
+        this(new DefaultComparator<K>());
+    }
+
+    protected int compare(Entry<K, V> a, Entry<K, V> b) {
+        return comp.compare(a.getKey(), b.getKey());
+    }
+
+    protected boolean checkKey(K key) throws IllegalArgumentException {
+        try {
+            return (com.compare(key, key) == 0);
+        } catch (ClassCastException e) {
+            throw new IllegalArgumentException("Incompatible key");
+        }
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
 }
