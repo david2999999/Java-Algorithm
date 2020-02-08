563e3d43ce30ca9d8a678d3da3fc89f079ead61f
David Jiang <djiang86@Binghamton.edu>
2020-02-08 11:24:32 -0500

    base priority heap implementation

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 9 - Priority Queues/Heaps/HeapPriorityQueue.java
@@ -0,0 +1,37 @@
+public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
+    protected ArrayList<Entry<K, V>> heap = new ArrayList<>();
+
+    public HeapPriorityQueue() {
+        super();
+    }
+
+    public HeapPriorityQueue(Comparator<K> comp) {
+        super(comp);
+    }
+
+    protected int parent(int j) {
+        return (j - 1) / 2;
+    }
+
+    protected int left(int j) {
+        return (2 * j) + 1;
+    }
+
+    protected int right(int j) {
+        return (2 * j) + 2;
+    }
+
+    protected boolean hasLeft(int j) {
+        return left(j) < heap.size();
+    }
+
+    protected boolean hasRight(int j) {
+        return right(j) < heap.size();
+    }
+
+    protected void swap(int i, int j) {
+        Entry<K, V> temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
+}
