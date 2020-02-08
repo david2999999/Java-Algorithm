60b42c4f1e992d3878160b4fde90b4e5288fc0bf
David Jiang <djiang86@Binghamton.edu>
2020-02-07 20:07:36 -0500

    Node class for binary tree

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 8 - Trees/LinkedBinaryTree.java
@@ -0,0 +1,47 @@
+public class LinkedBinaryTree<E> extends AbstractBinaryTree<E> {
+    protected static class Node<E> implements Position<E> {
+        private E element;
+        private Node<E> parent;
+        private Node<E> left;
+        private Node<E> right;
+
+        public Node(E e, Node<E> above, Node<E> leftChild, Node<E> rightChild) {
+            this.element = e;
+            this.parent = above;
+            this.left = leftChild;
+            this.right = rightChild;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public Node<E> getParent() {
+            return parent;
+        }
+
+        public void setParent(Node<E> parent) {
+            this.parent = parent;
+        }
+
+        public Node<E> getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node<E> left) {
+            this.left = left;
+        }
+
+        public Node<E> getRight() {
+            return right;
+        }
+
+        public void setRight(Node<E> right) {
+            this.right = right;
+        }
+    }
+}
