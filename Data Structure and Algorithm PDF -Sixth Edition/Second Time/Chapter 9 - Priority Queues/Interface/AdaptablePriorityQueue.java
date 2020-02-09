91a5b705f2306391817e1fae8e40e99e47adff3f
David Jiang <djiang86@Binghamton.edu>
2020-02-08 20:11:15 -0500

    adaptable priority queue interface

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 9 - Priority Queues/Interface/AdaptablePriorityQueue.java
@@ -0,0 +1,5 @@
+public interface AdaptablePriorityQueue<K, V> {
+    void remove(Entry<K, V> entry);
+    void replaceKey(Entry<K, V> entry, K key) throws IllegalArgumentException;
+    void replaceValue(Entry<K, V> entry, V value) throws IllegalArgumentException;
+}
