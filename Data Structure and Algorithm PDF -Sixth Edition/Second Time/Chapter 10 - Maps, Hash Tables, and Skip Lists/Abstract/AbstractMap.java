96b62fff767deb0939fe4823f337301b0ed97025
David Jiang <djiang86@Binghamton.edu>
2020-02-09 06:48:14 -0500

    protected map entry class for maps

---
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 10 - Maps, Hash Tables, and Skip Lists/Abstract/AbstractMap.java
@@ -0,0 +1,35 @@
+public abstract class AbstractMap<K, V> implements Map<K, V> {
+
+    protected static class MapEntry<K, V> implements Entry<K, V> {
+        private K key;
+        private V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public V setValue(V value) {
+            V old = this.value;
+            this.value = value;
+            return old;
+        }
+    }
+
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+}
