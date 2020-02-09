8428629ea9a7eec221d6d4a7a4b6b219482b07a6
David Jiang <djiang86@Binghamton.edu>
2020-02-09 06:54:35 -0500

    keyset iterable for map

--- Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 10 - Maps, Hash Tables, and Skip Lists/Abstract/AbstractMap.java
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 10 - Maps, Hash Tables, and Skip Lists/Abstract/AbstractMap.java
@@ -28,8 +28,33 @@ public abstract class AbstractMap<K, V> implements Map<K, V> {
         }
     }
 
-
     public boolean isEmpty() {
         return size() == 0;
+    }
+
+    private class KeyIterator implements Iterator<K> {
+        private Iterator<Entry<K, V>> entries = entrySet().iterator();
+
+        public boolean hasNext() {
+            return entries.hasNext();
+        }
+
+        public K next() {
+            return entries.next().getKey();
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    private class KeyIterable implements Iterable<K> {
+        public Iterator<K> iterator() {
+            return new KeyIterator();
+        }
+    }
+
+    public Iterable<K> keySet() {
+        return new KeyIterable();
     }
 }
