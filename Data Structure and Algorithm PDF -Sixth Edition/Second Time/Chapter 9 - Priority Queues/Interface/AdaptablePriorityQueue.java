1094a4367b0c92f75e88a57330c9a9b85416ef25
David Jiang <djiang86@Binghamton.edu>
2020-02-09 06:40:39 -0500

    adaptable priority queue interface extends priority queue interface

--- Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 9 - Priority Queues/Interface/AdaptablePriorityQueue.java
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 9 - Priority Queues/Interface/AdaptablePriorityQueue.java
@@ -1,4 +1,4 @@
-public interface AdaptablePriorityQueue<K, V> {
+public interface AdaptablePriorityQueue<K, V> extends PriorityQueue<K, V> {
     void remove(Entry<K, V> entry);
     void replaceKey(Entry<K, V> entry, K key) throws IllegalArgumentException;
     void replaceValue(Entry<K, V> entry, V value) throws IllegalArgumentException;
