db224180e238c4d916322c97013763189ace5918
david2999999 <djiang86@binghamton.edu>
2020-02-10 07:25:02 -0500

    project class for build order

---
+++ Cracking the Code Interview/Redo 5/Chapter 4 - Trees and Graphs/Build Order/Project.java
@@ -0,0 +1,35 @@
+public class Project {
+    private String name;
+    private int dependencies = 0;
+
+    private ArrayList<Project> children = new ArrayList<>();
+    private HashMap<String, Project> map = new HashMap<>();
+
+    public void addNeighbor(Project node) {
+        if (!map.containsKey(node.getName())) {
+            map.put(node.getName(), node);
+            children.add(node);
+            node.incrementDependencies();
+        }
+    }
+
+    public ArrayList<Project> getChildren() {
+        return children;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void incrementDependencies() {
+        dependencies++;
+    }
+
+    public void decrementDependencies() {
+        dependencies--;
+    }
+
+    public int getNumberOfDependencies() {
+        return dependencies;
+    }
+}
