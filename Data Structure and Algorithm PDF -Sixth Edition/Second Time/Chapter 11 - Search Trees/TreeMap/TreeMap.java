dc70b49d57457d1ba55dc2d9e580de8350b35ea6
David Jiang <djiang86@Binghamton.edu>
2020-02-10 20:29:08 -0500

    get, put, remove in tree map

--- Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 11 - Search Trees/TreeMap/TreeMap.java
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 11 - Search Trees/TreeMap/TreeMap.java
@@ -40,4 +40,67 @@ public class TreeMap<K, V> extends AbstractSortedMap<K, V> {
             return treeSearch(right(p), key);
         }
     }
+
+    public V get(K key) throws IllegalArgumentException {
+        checkKey(key);
+        Position<Entry<K, V>> position = treeSearch(root(), key);
+        rebalanceAccess(position);
+
+        if (isExternal(position)) {
+            return null;
+        }
+
+        return position.getElement().getValue();
+    }
+
+    public V put(K key, V value) throw IllegalArgumentException {
+        checkKey(key);
+        Entry<K, V> newEntry = new MapEntry<>(key, value);
+        Position<Entry<K, V>> position = treeSearch(root(), key);
+
+        if (isExternal(position)) {
+            expandExternal(position, newEntry);
+            rebalanceInsert(p);
+            return null;
+        } else {
+            V old = position.getElement().getValue();
+            set(position, newEntry);
+            rebalanceAccess(p);
+            return old;
+        }
+    }
+
+    public V remove(K key) throws IllegalArgumentException {
+        checkKey(key);
+        Position<Entry<K, V>> position = treeSearch(root(), key);
+
+        if (isExternal(position)) {
+            rebalanceAccess(p);
+            return null;
+        } else {
+            V old = position.getElement().getValue();
+            if (isInternal(left(position)) && isInternal(right(position))) {
+                Position<Entry<K, V>> replacement = treeMax(left(p));
+                set(position, replacement.getElement());
+                position = replacement;
+            }
+
+            Position<Entry<K, V>> leaf = (isExternal(left(p)) ? left(p) : right(p));
+            Position<Entry<K, V>> sibling = sibling(leaf);
+            remove(leaf);
+            remove(p);
+            rebalanceDelete(sibling);
+            return old;
+        }
+
+        protected Position<Entry<K, V>> treeMax(Position<Entry<K, V>> p) {
+            Position<Entry<K, V>> walk = p;
+
+            while (isInternal(walk)) {
+                walk = walk.right;
+            }
+
+            return parent(walk);
+        }
+    }
 }
