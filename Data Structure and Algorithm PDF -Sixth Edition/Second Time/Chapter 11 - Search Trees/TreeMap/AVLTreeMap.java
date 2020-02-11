06af92574e134217d3a113c1addfc6add7fad50b
David Jiang <djiang86@Binghamton.edu>
2020-02-11 18:04:00 -0500

    rebalancing tree after insertion and removal

--- Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 11 - Search Trees/TreeMap/AVLTreeMap.java
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 11 - Search Trees/TreeMap/AVLTreeMap.java
@@ -30,4 +30,31 @@ public class AVLTreeMap<K, V> extends TreeMap<K, V> {
             return right(p);
         }
     }
+
+    protected void rebalance(Position<Entry<K, V>> p) {
+        int oldHeight, newHeight;
+
+        do {
+            oldHeight = height(p);
+            if (!isBalanced(p)) {
+                p = restructure(tallerChild(tallerChild(p)));
+                recomputeHeight(left(p));
+                recomputeHeight(right(p));
+            }
+
+            recomputeHeight(p);
+            newHeight = height(p);
+            p = parent(p);
+        } while (oldHeight != newHeight && p != null);
+    }
+
+    protected void rebalanceInsert(Position<Entry<K, V>> p) {
+        rebalance(p);
+    }
+
+    protected void rebalanceDelete(Position<Entry<K, V>> p) {
+        if (!isRoot(p)) {
+            rebalance(parent(p));
+        }
+    }
 }
