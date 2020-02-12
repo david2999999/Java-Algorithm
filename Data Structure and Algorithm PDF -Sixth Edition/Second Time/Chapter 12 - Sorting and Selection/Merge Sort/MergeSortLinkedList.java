ce6ced7ff73f62eafa4a5dedee3623e6f833e5b4
David Jiang <djiang86@Binghamton.edu>
2020-02-11 20:38:55 -0500

    merge sort linked list

--- Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 12 - Sorting and Selection/Merge Sort/MergeSortLinkedList.java
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 12 - Sorting and Selection/Merge Sort/MergeSortLinkedList.java
@@ -1,4 +1,24 @@
 public class MergeSortLinkedList {
+    public <E> void mergeSort(Queue<K> queue, Comparator<K> comp) {
+        int length = queue.size();
+        if (length < 2) return;
+
+        Queue<K> buffer1 = new LinkedQueue<>();
+        Queue<K> buffer2 = new LinkedQueue<>();
+
+        while (buffer1.size() < length / 2) {
+            buffer1.enqueue(queue.dequeue());
+        }
+
+        while (!queue.isEmpty()) {
+            buffer2.enqueue(queue.dequeue());
+        }
+
+        mergeSort(buffer1, comp);
+        mergeSort(buffer2, comp);
+        merge(buffer1, buffer2, queue, comp);
+    }
+
     public <E> void merge(Queue<K> buffer1, Queue<K> buffer2,
                           Queue<K> queue, Comparator<K> comp) {
         while (!buffer1.isEmpty() && !buffer2.isEmpty()) {
