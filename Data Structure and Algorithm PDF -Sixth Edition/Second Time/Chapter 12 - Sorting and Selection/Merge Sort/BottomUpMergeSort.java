7353edd38e42bcf265fd8c4c7a518e29ba9b1541
David Jiang <djiang86@Binghamton.edu>
2020-02-11 21:23:06 -0500

    merge method for non recursive merge sort

--- Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 12 - Sorting and Selection/Merge Sort/BottomUpMergeSort.java
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 12 - Sorting and Selection/Merge Sort/BottomUpMergeSort.java
@@ -19,6 +19,31 @@ public class BottomUpMergeSort {
         }
     }
 
+    public <K> void merge(K[] source, K[] destination, Comparator<K> comp,
+                          int startIndex, int increments) {
+        int end1 = Math.min(startIndex + increments, source.length);
+        int end2 = Math.min(startIndex + 2 * increments, source.length);
+
+        int index1 = startIndex;
+        int index2 = startIndex + increments;
+
+        int accumulatorIndex = startIndex;
+
+        while (index1 < end1 && index2 < end2) {
+            if (comp.compare(source[index1], source[index2]) < 0) {
+                destination[accumulatorIndex++] = source[index1++];
+            } else {
+                destination[accumulatorIndex++] = source[index2++];
+            }
+        }
+
+        if (index1 < end1) {
+            System.arraycopy(source, index1, destination, accumulatorIndex, end1 - index1);
+        } else if (index2 < end2) {
+            System.arraycopy(source, index2, destination, accumulatorIndex, end2 - index2);
+        }
+    }
+
     public <E> void swap(K[] source, K[] destination) {
         K[] temp = source;
         source = destination;
