a807193027cd1a8ba3b7f502c484f4340f163ac1
David Jiang <djiang86@Binghamton.edu>
2020-02-12 22:23:59 -0500

    KMP algorithm

--- Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 13 - Text Processing/Pattern Matching Algorithms/Knuth-Morris-Pratt Algorithm/KMP.java
+++ Data Structure and Algorithm PDF -Sixth Edition/Second Time/Chapter 13 - Text Processing/Pattern Matching Algorithms/Knuth-Morris-Pratt Algorithm/KMP.java
@@ -1,4 +1,33 @@
 public class KMP {
+    public int findKMP(char[] text, char[] pattern) {
+        int textLength = text.length;
+        int patternLength = pattern.length;
+
+        if (patternLength == 0) return 0;
+
+        int[] failTable(pattern);
+
+        int j = 0;
+        int k = 0;
+
+        while (j < n) {
+            if (text[j] == pattern[k]) {
+                if (k == patternLength - 1) {
+                    return j - patternLength + 1;
+                }
+
+                k++;
+                j++;
+            } else if (k > 0) {
+                k = failTable[k - 1];
+            } else {
+                j++;
+            }
+        }
+
+        return -1;
+    }
+
 
     private int[] computeKMPFunction(char[] pattern) {
         int patternLength = pattern.length;
