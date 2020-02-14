eeaf3c6f958f0d5b3e8815bd360956133ae1fc0a
david2999999 <djiang86@binghamton.edu>
2020-02-14 06:08:13 -0500

    tree with random node access based on the probability of the size of subtrees

--- Cracking the Code Interview/Redo 5/Chapter 4 - Trees and Graphs/Random Node/Probability Based on Size/TreeNode.java
+++ Cracking the Code Interview/Redo 5/Chapter 4 - Trees and Graphs/Random Node/Probability Based on Size/TreeNode.java
@@ -1,5 +1,5 @@
 public class TreeNode {
-    private static final class Random random = new Random();
+    private static final Random random = new Random();
     private int data;
     public TreeNode left;
     public TreeNode right;
@@ -13,5 +13,39 @@ public class TreeNode {
     public TreeNode getRandomNode() {
         int leftTreeSize = this.left == null ? 0 : this.left.size;
         int index = random.nextInt(size);
+
+        if (index < leftTreeSize) {
+            return left.getRandomNode();
+        } else (index == leftTreeSize) {
+            return this;
+        } else {
+            return right.getRandomNode();
+        }
+    }
+
+    public void insertInOrder(int d) {
+        if (d <= data) {
+            if (left == null) {
+                left = new TreeNode(d);
+            } else {
+                left.insertInOrder(d);
+            }
+        } else {
+            if (right == null) {
+                right = new TreeNode(d);
+            } else {
+                right.insertInOrder(d);
+            }
+        }
+
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int data() {
+        return data;
     }
 }
