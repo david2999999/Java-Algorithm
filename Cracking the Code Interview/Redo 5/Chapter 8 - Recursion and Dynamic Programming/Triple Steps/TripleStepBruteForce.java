682c09306325e9257a68c99b55eb8a4d3343cd58
david2999999 <djiang86@binghamton.edu>
2020-02-18 06:15:48 -0500

    triple step brute force

---
+++ Cracking the Code Interview/Redo 5/Chapter 8 - Recursion and Dynamic Programming/Triple Steps/TripleStepBruteForce.java
@@ -0,0 +1,11 @@
+public class TripleStepBruteForce {
+    public int countWays(int n) {
+        if (n < 0) {
+            return 0;
+        } else if (n == 0) {
+            return 1;
+        } else {
+            return countWays(n - 1) + countWays(n - 2) + countWays(n - 3);
+        }
+    }
+}
