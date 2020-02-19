16ad8a1889422bdae90e3c372a180153f0e2c226
David Jiang <djiang86@Binghamton.edu>
2020-02-18 21:52:18 -0500

    binary tree node and binary tree node with depth

---
+++ Interview Cake/Problems/Trees/Super Balance Binary Tree/NodeDepthPair.java
@@ -0,0 +1,9 @@
+public class NodeDepthPair {
+    public BinaryTreeNode node;
+    public int depth;
+
+    public NodeDepthPair(BinaryTreeNode node, int depth) {
+        this.node = node;
+        this.depth = depth;
+    }
+}
