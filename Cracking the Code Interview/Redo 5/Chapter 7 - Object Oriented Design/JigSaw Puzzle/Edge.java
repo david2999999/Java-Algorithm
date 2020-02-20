4f28ee032034737d1002fd3de9bd81009630549a
David Jiang <djiang86@Binghamton.edu>
2020-02-20 05:45:38 -0500

    piece and edge object for single piece puzzles

---
+++ Cracking the Code Interview/Redo 5/Chapter 7 - Object Oriented Design/JigSaw Puzzle/Edge.java
@@ -0,0 +1,10 @@
+public class Edge {
+    private Shape shape;
+    private Piece parentPiece;
+
+    public Edge(Shape shape) {
+        this.shape = shape;
+    }
+
+    public boolean fitsWith(Edge edge) {}
+}
---
+++ Cracking the Code Interview/Redo 5/Chapter 7 - Object Oriented Design/JigSaw Puzzle/Piece.java
@@ -0,0 +1,9 @@
+public class Piece {
+    private HashMap<Orientation, Edge> edges = new HashMap<>();
+    public Piece(Edge[] edgeList) {}
+
+    public void rotateEdgesBy(int numberRotations) {}
+
+    public boolean isCorner() {}
+    public boolean isBorder() {}
+}
