f2d4b8bdd91435187d894c323fe6eac6c7f261e8
david2999999 <djiang86@binghamton.edu>
2020-02-21 08:31:17 -0500

    generating all possible parens recursively

---
+++ Cracking the Code Interview/Redo 5/Chapter 8 - Recursion and Dynamic Programming/Parens/ParenthesesRecursive.java
@@ -0,0 +1,22 @@
+public class ParenthesesRecursive {
+    public ArrayList<String> generateParens(int count) {
+        char[] str = new char[count * 2];
+        ArrayList<String> list = new ArrayList<>();
+        addParens(list, count, count, str, 0);
+        return list;
+    }
+
+    private void addParens(ArrayList<String> list, int leftRem, int rightRem, char[] str, int index) {
+        if (leftRem < 0 || rightRem < leftRem) return;
+
+        if (leftRem == 0 && rightRem == 0) {
+            list.add(new String(str));
+        } else {
+            str[index] = '(';
+            addParens(list, leftRem - 1, rightRem, str, index + 1);
+
+            str[index] = ')';
+            addParens(list, leftRem, rightRem - 1, str, index + 1);
+        }
+    }
+}
