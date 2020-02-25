5cfd4f67cc407f95ad597e4c6d5d5f21adfb5e5e
david2999999 <djiang86@binghamton.edu>
2020-02-25 08:10:58 -0500

    maximum height of stack of boxes recursive solution

---
+++ Cracking the Code Interview/Redo 5/Chapter 8 - Recursion and Dynamic Programming/Stack of Boxes/StackOfBoxesRecursion.java
@@ -0,0 +1,35 @@
+public class StackOfBoxesRecursion {
+    public int createStack(ArrayList<Box> boxes) {
+        Collections.sort(boxes, new BoxComparator());
+
+        int maxHeight = 0;
+        for (int i = 0; i < boxes.size(); i++) {
+            int height = getMaxHeight(boxes, i);
+            maxHeight = Math.max(height, maxHeight);
+        }
+
+        return maxHeight;
+    }
+
+    private int getMaxHeight(ArrayList<Box> boxes, int bottomIndex) {
+        Box bottomBox = boxes.get(bottomIndex);
+        int maxHeight = 0;
+
+        for (int i = bottomIndex + 1; i < boxes.size(); i++) {
+            if (boxes.get(i).canBeOnTop(bottomBox)) {
+                int height = getMaxHeight(boxes, i);
+                maxHeight = Math.max(height, maxHeight);
+            }
+        }
+
+        maxHeight += bottomBox.height;
+        return maxHeight;
+    }
+
+    public class BoxComparator implements Comparator<Boxes> {
+        @Override
+        public int compare(Box b1, Box b2) {
+            return b2.height - b1.height;
+        }
+    }
+}
