4f5879994a1bac40339190349326914dbaba4f69
david2999999 <djiang86@binghamton.edu>
2020-02-27 06:39:42 -0500

    single level breath first search and finding collision

--- Cracking the Code Interview/Redo 5/Chapter 9 - System Design and Scalability/Social Network/SocialNetwork.java
+++ Cracking the Code Interview/Redo 5/Chapter 9 - System Design and Scalability/Social Network/SocialNetwork.java
@@ -10,14 +10,39 @@ public class SocialNetwork {
         BFSData destData = new BFSData(people.get(destination));
 
         while (!sourceData.isFinished() && !destData.isFinished()) {
-            Person collision = searchLevel(people, sourceData, destData);
+            Person collision = searchLevel(sourceData, destData);
             if (collision != null) {
                 return mergePaths(sourceData, destData, collision.getID());
             }
 
-            collision = searchLevel(people, destData, sourceData);
+            collision = searchLevel(destData, sourceData);
             if (collision != null) {
                 return mergePaths(sourceData, destData, collision.getID());
+            }
+        }
+
+        return null;
+    }
+
+    private Person searchLevel(BFSData primary, BFSData secondary) {
+        int count = primary.toVisit.size();
+        for (int i = 0; i < count; i++) {
+            PathNode pathNode = primary.toVisit.poll();
+            int personId = pathNode.getPerson.getID();
+
+            if (secondary.visited.containsKey(personId)) {
+                return pathNode.getPerson();
+            }
+
+            Person person = pathNode.getPerson();
+            ArrayList<Integer> friends = person.getFriends();
+            for (int friendId: friends) {
+                if (!primary.visited.containsKey(friendId)) {
+                    Person friend = person.get(friendId);
+                    PathNode next = new PathNode(friend, pathNode);
+                    primary.visited.put(friendId, next);
+                    primary.toVisit.add(next);
+                }
             }
         }
 
