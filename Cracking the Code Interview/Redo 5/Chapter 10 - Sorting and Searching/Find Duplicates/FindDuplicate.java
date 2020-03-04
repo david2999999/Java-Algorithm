b0ab9b9bbcbda1493d095e7ad45de6901b72395f
david2999999 <djiang86@binghamton.edu>
2020-03-04 07:28:21 -0500

    using bit set to find the duplicate element inside an array

--- Cracking the Code Interview/Redo 5/Chapter 10 - Sorting and Searching/Find Duplicates/FindDuplicate.java
+++ Cracking the Code Interview/Redo 5/Chapter 10 - Sorting and Searching/Find Duplicates/FindDuplicate.java
@@ -1,5 +1,18 @@
 public class FindDuplicate {
-    public void checkDuplicates(int[] array) {
+    private static final int MAX_VALUE = 32000;
 
+    public void checkDuplicates(int[] array) {
+        BitSet bs = new BitSet(MAX_VALUE);
+
+        for (int i = 0; i < array.length; i++) {
+            int num = array[i];
+            int num0 = num - 1;
+
+            if (bs.getBit(num0)) {
+                System.out.println(num);
+            } else {
+                bs.set(num0);
+            }
+        }
     }
 }
