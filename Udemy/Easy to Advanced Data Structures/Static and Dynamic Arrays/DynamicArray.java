27422e02eaa1c0e1307cf61ddd6f52358e8e5819
David Jiang <djiang86@binghamton.edu>
2020-03-07 08:55:28 -0500

    initial implementation of dynamic array

---
+++ Udemy/Easy to Advanced Data Structures/Static and Dynamic Arrays/DynamicArray.java
@@ -0,0 +1,42 @@
+public class DynamicArray<T> implements Iterable<T> {
+    private T[] arr;
+    private int length = 0;
+    private int capacity = 0;
+
+    public DynamicArray() {
+        this(16);
+    }
+
+    public DynamicArray(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Illegal Capacity: " + capacity);
+        }
+
+        this.capacity = capacity;
+        arr = (T[]) new Object[capacity];
+    }
+
+    public int size() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public T get(int index) {
+        return arr[index];
+    }
+
+    public T set(int index, T element) {
+        arr[index] = element;
+    }
+
+    public void clear() {
+        for (int i = 0; i < capacity; i++) {
+            arr[i] = null;
+        }
+
+        this.length = 0;
+    }
+}
